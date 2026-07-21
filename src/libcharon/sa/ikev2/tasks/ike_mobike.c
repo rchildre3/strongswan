4c3e5330c506a874bbc0fe337bfca9ab38164fd5
Tobias Brunner <tobias@strongswan.org>
2026-07-21 11:52:46 +0200

    chunk: Improve constant time comparison for chunks with unequal length
    While for most uses the length is fixed and public (e.g. PRF/MAC outputs),
    there are a few (e.g. in xauth-generic) that compare variable length
    data.
    
    The previous code directly leaked a differing length by short-circuiting
    before comparing anything.  While we could limit the comparison by the
    minimum length (and call `memeq_const()`), that could still leak the
    length because the time will plateau once the secret's length is reached.
    Similarly, if the comparison was bound by the longer chunk (would prevent
    the use of `memeq_const()`), the length could also be revealed once the
    input gets longer than the secret and the time increases.
    
    This changes the semantics of the function by declaring the first
    argument the expected/reference secret and the second the variable input.
    This strictly makes the function constant-time, bound by the secret's
    length.  So the length can't be guessed by providing different input (but
    if an attacker can trigger the comparison against different secrets, of
    potentially known lengths, it might still be possible).  If the chunks
    are known to have the same length, the order doesn't matter.
    
    Callers of this function have been updated accordingly.

--- src/libcharon/sa/ikev2/tasks/ike_mobike.c
+++ src/libcharon/sa/ikev2/tasks/ike_mobike.c
@@ -523,7 +523,7 @@ METHOD(task_t, process_i, status_t,
 			cookie2 = this->cookie2;
 			this->cookie2 = chunk_empty;
 			process_payloads(this, message);
-			if (!chunk_equals_const(cookie2, this->cookie2))
+			if (!chunk_equals_const(this->cookie2, cookie2))
 			{
 				chunk_free(&cookie2);
 				DBG1(DBG_IKE, "COOKIE2 mismatch, closing IKE_SA");
