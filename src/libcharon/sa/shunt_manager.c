rchildre3/strongswan

commit 616ff9a2369fd250a2b9e8d2a00f37e2e8d3a2f3

shunt-manager: Remove stored entries if installation fails

src/libcharon/sa/shunt_manager.c
@@ -122,7 +122,7 @@ METHOD(shunt_manager_t, install, bool,
 {
 	enumerator_t *enumerator;
 	child_cfg_t *child_cfg;
-	bool found = FALSE;
+	bool found = FALSE, success;
 
 	/* check if not already installed */
 	this->lock->write_lock(this->lock);
@@ -146,7 +146,16 @@ METHOD(shunt_manager_t, install, bool,
 	this->shunts->insert_last(this->shunts, child->get_ref(child));
 	this->lock->unlock(this->lock);
 
-	return install_shunt_policy(child);
+	success = install_shunt_policy(child);
+
+	if (!success)
+	{
+		this->lock->write_lock(this->lock);
+		this->shunts->remove(this->shunts, child, NULL);
+		this->lock->unlock(this->lock);
+		child->destroy(child);
+	}
+	return success;
 }
 
 /**

commit 6b1ad0cf3332ee8f4ba7ffec2ab39f82770c328e

shunt-manager: Fix potential race between install and uninstall
If the installation fails while a shunt is concurrently uninstalled,
the entry could already be destroyed when trying to remove and destroy
it after acquiring the lock again in `install()`.

This change handles the conflict the same way trap-manager does since
69cbe2ca3fe4 ("trap-manager: Wait for install to finish before
uninstalling").

Fixes: 616ff9a2369f ("shunt-manager: Remove stored entries if installation fails")

src/libcharon/sa/shunt_manager.c
@@ -377,6 +377,10 @@ METHOD(shunt_manager_t, uninstall, bool,
 	entry_t *entry, *found = NULL;
 
 	this->lock->write_lock(this->lock);
+	while (this->installing)
+	{
+		this->condvar->wait(this->condvar, this->lock);
+	}
 	enumerator = this->shunts->create_enumerator(this->shunts);
 	while (enumerator->enumerate(enumerator, &entry))
 	{