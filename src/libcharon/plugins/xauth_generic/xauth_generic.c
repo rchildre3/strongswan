rchildre3/strongswan

commit 462c9a4f72ec94ae993863341502d78a411f4e71

Try all matching XAuth secrets we find, not only the first one

src/libcharon/plugins/xauth_generic/xauth_generic.c
@@ -100,7 +100,8 @@ METHOD(xauth_method_t, process_server, status_t,
 	shared_key_t *shared;
 	identification_t *id;
 	chunk_t user = chunk_empty, pass = chunk_empty;
-	status_t status = SUCCESS;
+	status_t status = FAILED;
+	int tried = 0;
 
 	enumerator = in->create_attribute_enumerator(in);
 	while (enumerator->enumerate(enumerator, &attr))
@@ -136,20 +137,31 @@ METHOD(xauth_method_t, process_server, status_t,
 		this->peer = id;
 	}
 
-	shared = lib->credmgr->get_shared(lib->credmgr, SHARED_EAP,
-									  this->server, this->peer);
-	if (!shared)
+	enumerator = lib->credmgr->create_shared_enumerator(lib->credmgr,
+										SHARED_EAP, this->server, this->peer);
+	while (enumerator->enumerate(enumerator, &shared, NULL, NULL))
 	{
-		DBG1(DBG_IKE, "no XAuth secret found for '%Y' - '%Y'",
-			 this->server, this->peer);
-		status = FAILED;
+		if (chunk_equals(shared->get_key(shared), pass))
+		{
+			status = SUCCESS;
+			break;
+		}
+		tried++;
 	}
-	else if (!chunk_equals(shared->get_key(shared), pass))
+	enumerator->destroy(enumerator);
+	if (status != SUCCESS)
 	{
-		DBG1(DBG_IKE, "failed to authenticate '%Y' with XAuth", this->peer);
-		status = FAILED;
+		if (!tried)
+		{
+			DBG1(DBG_IKE, "no XAuth secret found for '%Y' - '%Y'",
+				 this->server, this->peer);
+		}
+		else
+		{
+			DBG1(DBG_IKE, "none of %d found XAuth secrets for '%Y' - '%Y' "
+				 "matched", tried, this->server, this->peer);
+		}
 	}
-	DESTROY_IF(shared);
 	return status;
 }
 

commit ab476be10c6dfe6b3d2a59d879990ada0bc89207

xauth-generic: Make sure to only use secrets that match the username
This ensures that secrets that are associated with the server identity,
usually they are assigned to the username exclusively, will only be tried
if the username also matched.  Otherwise, it could be possible for users
who know at least one password to impersonate another user.

Fixes: 462c9a4f72ec ("Try all matching XAuth secrets we find, not only the first one")

src/libcharon/plugins/xauth_generic/xauth_generic.c
@@ -134,6 +134,7 @@ METHOD(xauth_method_t, process_server, status_t,
 	configuration_attribute_t *attr;
 	enumerator_t *enumerator;
 	shared_key_t *shared;
+	id_match_t match_other;
 	identification_t *id;
 	chunk_t user = chunk_empty, pass = chunk_empty;
 	status_t status = FAILED;
@@ -179,9 +180,10 @@ METHOD(xauth_method_t, process_server, status_t,
 
 	enumerator = lib->credmgr->create_shared_enumerator(lib->credmgr,
 										SHARED_EAP, this->server, this->peer);
-	while (enumerator->enumerate(enumerator, &shared, NULL, NULL))
+	while (enumerator->enumerate(enumerator, &shared, NULL, &match_other))
 	{
-		if (chunk_equals_const(shared->get_key(shared), pass))
+		if (match_other > ID_MATCH_NONE &&
+			chunk_equals_const(shared->get_key(shared), pass))
 		{
 			status = SUCCESS;
 			break;