rchildre3/strongswan

commit c92eade82c814e9f6b55f80d715bb51d28d5e1fb

wolfssl: Add wolfSSL plugin for cryptographic implementations

src/libstrongswan/plugins/wolfssl/wolfssl_ec_private_key.c
@@ -1,0 +1,508 @@
+/*
+ * Copyright (C) 2019 Sean Parkinson, wolfSSL Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "wolfssl_common.h"
+
+#ifdef HAVE_ECC_SIGN
+
+#include "wolfssl_ec_private_key.h"
+#include "wolfssl_ec_public_key.h"
+#include "wolfssl_util.h"
+
+#include <asn1/asn1.h>
+#include <asn1/oid.h>
+
+#include <utils/debug.h>
+
+#include <wolfssl/wolfcrypt/ecc.h>
+
+
+typedef struct private_wolfssl_ec_private_key_t private_wolfssl_ec_private_key_t;
+
+/**
+ * Private data of a wolfssl_ec_private_key_t object.
+ */
+struct private_wolfssl_ec_private_key_t {
+	/**
+	 * Public interface for this signer.
+	 */
+	wolfssl_ec_private_key_t public;
+
+	/**
+	 * Key size
+	 */
+	int keysize;
+
+	/**
+	 * EC key object
+	 */
+	ecc_key ec;
+
+	/**
+	 * Random number generator
+	 */
+	WC_RNG rng;
+
+	/**
+	 * reference count
+	 */
+	refcount_t ref;
+};
+
+/* from ec public key */
+bool wolfssl_ec_fingerprint(ecc_key *ec, cred_encoding_type_t type, chunk_t *fp);
+
+/**
+ * Build a signature as in RFC 4754
+ */
+static bool build_signature(private_wolfssl_ec_private_key_t *this,
+							chunk_t hash, chunk_t *signature)
+{
+	bool success = FALSE;
+	mp_int r, s;
+
+	if (mp_init(&r) != 0)
+	{
+		return FALSE;
+	}
+	if (mp_init(&s) != 0)
+	{
+		mp_free(&r);
+		return FALSE;
+	}
+	if (wc_ecc_sign_hash_ex(hash.ptr, hash.len, &this->rng, &this->ec, &r,
+							&s) == 0)
+	{
+		success = wolfssl_mp_cat(this->ec.dp->size * 2, &r, &s, signature);
+	}
+
+	mp_free(&s);
+	mp_free(&r);
+	
+	return success;
+}
+
+/**
+ * Build a RFC 4754 signature for a specified curve and hash algorithm
+ */
+static bool build_curve_signature(private_wolfssl_ec_private_key_t *this,
+		signature_scheme_t scheme, enum wc_HashType hash, ecc_curve_id curve_id,
+		chunk_t data, chunk_t *signature)
+{
+	bool success = FALSE;
+	chunk_t dgst = chunk_empty;
+
+	if (curve_id != this->ec.dp->id)
+	{
+		DBG1(DBG_LIB, "signature scheme %N not supported by private key",
+			signature_scheme_names, scheme);
+		return FALSE;
+	}
+	if (wolfssl_hash_chunk(hash, data, &dgst))
+	{
+		success = build_signature(this, dgst, signature);
+	}
+	chunk_free(&dgst);
+	return success;
+}
+
+/**
+ * Build a DER encoded signature as in RFC 3279
+ */
+static bool build_der_signature(private_wolfssl_ec_private_key_t *this,
+		enum wc_HashType hash, chunk_t data, chunk_t *signature)
+{
+	bool success = FALSE;
+	chunk_t dgst = chunk_empty;
+	int ret;
+	word32 len;
+
+	if (wolfssl_hash_chunk(hash, data, &dgst))
+	{
+		*signature = chunk_alloc(this->ec.dp->size * 2 + 10);
+		len = signature->len;
+		ret = wc_ecc_sign_hash(dgst.ptr, dgst.len, signature->ptr, &len,
+							   &this->rng, &this->ec);
+		if (ret == 0)
+		{
+			signature->len = len;
+			success = TRUE;
+		}
+		else
+		{
+			chunk_free(signature);
+		}
+	}
+
+	chunk_free(&dgst);
+	return success;
+}
+
+METHOD(private_key_t, sign, bool,
+	private_wolfssl_ec_private_key_t *this, signature_scheme_t scheme,
+	void *params, chunk_t data, chunk_t *signature)
+{
+	switch (scheme)
+	{
+		case SIGN_ECDSA_WITH_NULL:
+			return build_signature(this, data, signature);
+	#ifndef NO_SHA
+		case SIGN_ECDSA_WITH_SHA1_DER:
+			return build_der_signature(this, WC_HASH_TYPE_SHA, data, signature);
+	#endif
+	#ifndef NO_SHA256
+		case SIGN_ECDSA_WITH_SHA256_DER:
+			return build_der_signature(this, WC_HASH_TYPE_SHA256, data,
+									   signature);
+	#endif
+	#ifdef WOLFSSL_SHA384
+		case SIGN_ECDSA_WITH_SHA384_DER:
+			return build_der_signature(this, WC_HASH_TYPE_SHA384, data,
+									   signature);
+	#endif
+	#ifdef WOLFSSL_SHA512
+		case SIGN_ECDSA_WITH_SHA512_DER:
+			return build_der_signature(this, WC_HASH_TYPE_SHA512, data,
+									   signature);
+	#endif
+	#ifndef NO_SHA256
+		case SIGN_ECDSA_256:
+			return build_curve_signature(this, scheme, WC_HASH_TYPE_SHA256,
+										 ECC_SECP256R1, data, signature);
+	#endif
+	#ifdef WOLFSSL_SHA384
+		case SIGN_ECDSA_384:
+			return build_curve_signature(this, scheme, WC_HASH_TYPE_SHA384,
+										 ECC_SECP384R1, data, signature);
+	#endif
+	#ifdef WOLFSSL_SHA512
+		case SIGN_ECDSA_521:
+			return build_curve_signature(this, scheme, WC_HASH_TYPE_SHA512,
+										 ECC_SECP521R1, data, signature);
+	#endif
+		default:
+			DBG1(DBG_LIB, "signature scheme %N not supported",
+				 signature_scheme_names, scheme);
+			return FALSE;
+	}
+}
+
+METHOD(private_key_t, decrypt, bool,
+	private_wolfssl_ec_private_key_t *this, encryption_scheme_t scheme,
+	chunk_t crypto, chunk_t *plain)
+{
+	DBG1(DBG_LIB, "EC private key decryption not implemented");
+	return FALSE;
+}
+
+METHOD(private_key_t, get_keysize, int,
+	private_wolfssl_ec_private_key_t *this)
+{
+	return this->keysize;
+}
+
+METHOD(private_key_t, get_type, key_type_t,
+	private_wolfssl_ec_private_key_t *this)
+{
+	return KEY_ECDSA;
+}
+
+METHOD(private_key_t, get_public_key, public_key_t*,
+	private_wolfssl_ec_private_key_t *this)
+{
+	public_key_t *public;
+	chunk_t key;
+	int ret;
+
+	key = chunk_alloc((this->keysize + 7) / 8 * 5);
+	ret = wc_EccPublicKeyToDer(&this->ec, key.ptr, key.len, 1);
+	if (ret < 0)
+	{
+		chunk_free(&key);
+		return NULL;
+	}
+	key.len = ret;
+
+	public = lib->creds->create(lib->creds, CRED_PUBLIC_KEY, KEY_ECDSA,
+								BUILD_BLOB_ASN1_DER, key, BUILD_END);
+	free(key.ptr);
+	return public;
+}
+
+METHOD(private_key_t, get_fingerprint, bool,
+	private_wolfssl_ec_private_key_t *this, cred_encoding_type_t type,
+	chunk_t *fingerprint)
+{
+	return wolfssl_ec_fingerprint(&this->ec, type, fingerprint);
+}
+
+METHOD(private_key_t, get_encoding, bool,
+	private_wolfssl_ec_private_key_t *this, cred_encoding_type_t type,
+	chunk_t *encoding)
+{
+	switch (type)
+	{
+		case PRIVKEY_ASN1_DER:
+		case PRIVKEY_PEM:
+		{
+			bool success = TRUE;
+
+			*encoding = chunk_alloc(this->keysize * 4 + 30);
+			if (wc_EccPrivateKeyToDer(&this->ec, encoding->ptr,
+									  encoding->len) < 0)
+			{
+				chunk_free(encoding);
+				return FALSE;
+			}
+
+			if (type == PRIVKEY_PEM)
+			{
+				chunk_t asn1_encoding = *encoding;
+
+				success = lib->encoding->encode(lib->encoding, PRIVKEY_PEM,
+								NULL, encoding, CRED_PART_ECDSA_PRIV_ASN1_DER,
+								asn1_encoding, CRED_PART_END);
+				chunk_clear(&asn1_encoding);
+			}
+			return success;
+		}
+		default:
+			return FALSE;
+	}
+}
+
+METHOD(private_key_t, get_ref, private_key_t*,
+	private_wolfssl_ec_private_key_t *this)
+{
+	ref_get(&this->ref);
+	return &this->public.key;
+}
+
+METHOD(private_key_t, destroy, void,
+	private_wolfssl_ec_private_key_t *this)
+{
+	if (ref_put(&this->ref))
+	{
+		lib->encoding->clear_cache(lib->encoding, &this->ec);
+		wc_FreeRng(&this->rng);
+		wc_ecc_free(&this->ec);
+		free(this);
+	}
+}
+
+/**
+ * Internal generic constructor
+ */
+static private_wolfssl_ec_private_key_t *create_empty(void)
+{
+	private_wolfssl_ec_private_key_t *this;
+
+	INIT(this,
+		.public = {
+			.key = {
+				.get_type = _get_type,
+				.sign = _sign,
+				.decrypt = _decrypt,
+				.get_keysize = _get_keysize,
+				.get_public_key = _get_public_key,
+				.equals = private_key_equals,
+				.belongs_to = private_key_belongs_to,
+				.get_fingerprint = _get_fingerprint,
+				.has_fingerprint = private_key_has_fingerprint,
+				.get_encoding = _get_encoding,
+				.get_ref = _get_ref,
+				.destroy = _destroy,
+			},
+		},
+		.ref = 1,
+	);
+
+	if (wc_InitRng(&this->rng) < 0)
+	{
+		DBG1(DBG_LIB, "Random number generation init failed");
+		free(this);
+		return NULL;
+	}
+
+	return this;
+}
+
+/*
+ * See header.
+ */
+wolfssl_ec_private_key_t *wolfssl_ec_private_key_gen(key_type_t type,
+													 va_list args)
+{
+	private_wolfssl_ec_private_key_t *this;
+	u_int key_size = 0;
+	ecc_curve_id curve_id;
+
+	while (TRUE)
+	{
+		switch (va_arg(args, builder_part_t))
+		{
+			case BUILD_KEY_SIZE:
+				key_size = va_arg(args, u_int);
+				continue;
+			case BUILD_END:
+				break;
+			default:
+				return NULL;
+		}
+		break;
+	}
+	if (!key_size)
+	{
+		return NULL;
+	}
+	this = create_empty();
+	this->keysize = key_size;
+	switch (key_size)
+	{
+		case 256:
+			curve_id = ECC_SECP256R1;
+			break;
+		case 384:
+			curve_id = ECC_SECP384R1;
+			break;
+		case 521:
+			curve_id = ECC_SECP521R1;
+			break;
+		default:
+			DBG1(DBG_LIB, "EC private key size %d not supported", key_size);
+			destroy(this);
+			return NULL;
+	}
+
+	if (wc_ecc_make_key_ex(&this->rng, (key_size + 7) / 8, &this->ec,
+						   curve_id) < 0)
+	{
+		DBG1(DBG_LIB, "EC private key generation failed", key_size);
+		destroy(this);
+		return NULL;
+	}
+	return &this->public;
+}
+
+/**
+ * See header.
+ */
+wolfssl_ec_private_key_t *wolfssl_ec_private_key_load(key_type_t type,
+													  va_list args)
+{
+	private_wolfssl_ec_private_key_t *this;
+	chunk_t params = chunk_empty;
+	chunk_t key = chunk_empty;
+	chunk_t alg_id = chunk_empty;
+	word32 idx;
+	int oid;
+
+	while (TRUE)
+	{
+		switch (va_arg(args, builder_part_t))
+		{
+			case BUILD_BLOB_ALGID_PARAMS:
+				params = va_arg(args, chunk_t);
+				continue;
+			case BUILD_BLOB_ASN1_DER:
+				key = va_arg(args, chunk_t);
+				continue;
+			case BUILD_END:
+				break;
+			default:
+				return NULL;
+		}
+		break;
+	}
+	if (key.ptr == NULL)
+	{
+		return NULL;
+	}
+
+	this = create_empty();
+
+	idx = 0;
+	if (wc_EccPrivateKeyDecode(key.ptr, &idx, &this->ec, key.len) < 0)
+	{
+		destroy(this);
+		return NULL;
+	}
+	switch (this->ec.dp->id)
+	{
+		case ECC_SECP256R1:
+			this->keysize = 256;
+			break;
+		case ECC_SECP384R1:
+			this->keysize = 384;
+			break;
+		case ECC_SECP521R1:
+			this->keysize = 521;
+			break;
+		default:
+			break;
+	}
+
+	if (params.ptr)
+	{
+		/* if ECParameters is passed, check we guessed correct */
+		alg_id = asn1_algorithmIdentifier_params(OID_EC_PUBLICKEY,
+												 chunk_clone(params));
+		if (asn1_unwrap(&params, &params) == ASN1_OID)
+		{
+			oid = asn1_known_oid(params);
+			switch (oid)
+			{
+				case OID_PRIME256V1:
+					if (this->ec.dp->id != ECC_SECP256R1)
+					{
+						oid = OID_UNKNOWN;
+					}
+					break;
+				case OID_SECT384R1:
+					if (this->ec.dp->id != ECC_SECP384R1)
+					{
+						oid = OID_UNKNOWN;
+					}
+					break;
+				case OID_SECT521R1:
+					if (this->ec.dp->id != ECC_SECP521R1)
+					{
+						oid = OID_UNKNOWN;
+					}
+					break;
+				default:
+					oid = OID_UNKNOWN;
+					break;
+			}
+		}
+		chunk_free(&alg_id);
+		if (oid == OID_UNKNOWN)
+		{
+			DBG1(DBG_LIB, "parameters do not match private key data");
+			destroy(this);
+			return NULL;
+		}
+	}
+
+	return &this->public;
+}
+#endif /* HAVE_ECC_SIGN */

commit df83a230f753e19e304f8d346edfa43e94d62975

wolfssl: Avoid potential RNG state corruption during RSA/ECDSA operations
The WC_RNG instances are potentially shared between different threads
as private key objects are refcounted.  This may corrupt their internal
state as they are not thread-safe.

For ECDSA, using separate instances for each signing operation has some
performance impact, but for signature operations that should be fine.

The implementation for RSA uses mutexes.  That's due to the weird API.
While RNG instances can be passed for signing and encryption (probably
because they are also required for padding/salt besides blinding), they
can't for verifying and decryption.  The latter use an RNG instance that
has to be set on the key object before calling these operations.  So we
could potentially split the strategy, but to keep this consistent within
the RSA implementation, just continue with the shared RNG but use a
mutex around the API calls.

Fixes: c92eade82c81 ("wolfssl: Add wolfSSL plugin for cryptographic implementations")

src/libstrongswan/plugins/wolfssl/wolfssl_ec_private_key.c
@@ -59,11 +59,6 @@ struct private_wolfssl_ec_private_key_t {
 	ecc_key ec;
 
 	/**
-	 * Random number generator
-	 */
-	WC_RNG rng;
-
-	/**
 	 * Reference count
 	 */
 	refcount_t ref;
@@ -79,25 +74,32 @@ static bool build_signature(private_wolfssl_ec_private_key_t *this,
 							chunk_t hash, chunk_t *signature)
 {
 	bool success = FALSE;
+	WC_RNG rng;
 	mp_int r, s;
 
+	if (wc_InitRng(&rng) != 0)
+	{
+		return FALSE;
+	}
 	if (mp_init(&r) != 0)
 	{
+		wc_FreeRng(&rng);
 		return FALSE;
 	}
 	if (mp_init(&s) != 0)
 	{
 		mp_free(&r);
+		wc_FreeRng(&rng);
 		return FALSE;
 	}
-	if (wc_ecc_sign_hash_ex(hash.ptr, hash.len, &this->rng, &this->ec, &r,
-							&s) == 0)
+	if (wc_ecc_sign_hash_ex(hash.ptr, hash.len, &rng, &this->ec, &r, &s) == 0)
 	{
 		success = wolfssl_mp_cat(this->ec.dp->size * 2, &r, &s, signature);
 	}
 
 	mp_free(&s);
 	mp_free(&r);
+	wc_FreeRng(&rng);
 	return success;
 }
 
@@ -135,14 +137,19 @@ static bool build_der_signature(private_wolfssl_ec_private_key_t *this,
 {
 	chunk_t dgst = chunk_empty;
 	bool success = FALSE;
+	WC_RNG rng;
 	word32 len;
 
+	if (wc_InitRng(&rng) != 0)
+	{
+		return FALSE;
+	}
 	if (wolfssl_hash_chunk(hash, data, &dgst))
 	{
 		*signature = chunk_alloc(wc_ecc_sig_size(&this->ec));
 		len = signature->len;
 		if (wc_ecc_sign_hash(dgst.ptr, dgst.len, signature->ptr, &len,
-							   &this->rng, &this->ec) == 0)
+							 &rng, &this->ec) == 0)
 		{
 			signature->len = len;
 			success = TRUE;
@@ -153,6 +160,7 @@ static bool build_der_signature(private_wolfssl_ec_private_key_t *this,
 		}
 	}
 	chunk_free(&dgst);
+	wc_FreeRng(&rng);
 	return success;
 }
 
@@ -306,7 +314,6 @@ METHOD(private_key_t, destroy, void,
 	if (ref_put(&this->ref))
 	{
 		lib->encoding->clear_cache(lib->encoding, &this->ec);
-		wc_FreeRng(&this->rng);
 		wc_ecc_free(&this->ec);
 		free(this);
 	}
@@ -345,13 +352,6 @@ static private_wolfssl_ec_private_key_t *create_empty(void)
 		free(this);
 		return NULL;
 	}
-	if (wc_InitRng(&this->rng) != 0)
-	{
-		DBG1(DBG_LIB, "RNG initialization for EC private key failed");
-		wc_ecc_free(&this->ec);
-		free(this);
-		return NULL;
-	}
 	return this;
 }
 
@@ -364,6 +364,7 @@ wolfssl_ec_private_key_t *wolfssl_ec_private_key_gen(key_type_t type,
 	private_wolfssl_ec_private_key_t *this;
 	u_int key_size = 0;
 	ecc_curve_id curve_id;
+	WC_RNG rng;
 
 	while (TRUE)
 	{
@@ -407,13 +408,19 @@ wolfssl_ec_private_key_t *wolfssl_ec_private_key_gen(key_type_t type,
 			return NULL;
 	}
 
-	if (wc_ecc_make_key_ex(&this->rng, (key_size + 7) / 8, &this->ec,
-						   curve_id) < 0)
+	if (wc_InitRng(&rng) != 0)
 	{
-		DBG1(DBG_LIB, "EC private key generation failed");
 		destroy(this);
 		return NULL;
 	}
+	if (wc_ecc_make_key_ex(&rng, (key_size + 7) / 8, &this->ec, curve_id) < 0)
+	{
+		DBG1(DBG_LIB, "EC private key generation failed");
+		destroy(this);
+		wc_FreeRng(&rng);
+		return NULL;
+	}
+	wc_FreeRng(&rng);
 	return &this->public;
 }
 

src/libstrongswan/plugins/wolfssl/wolfssl_rsa_private_key.c
@@ -58,6 +58,11 @@ struct private_wolfssl_rsa_private_key_t {
 	WC_RNG rng;
 
 	/**
+	 * Mutex to protect access to the RNG during API calls.
+	 */
+	wolfSSL_Mutex mutex;
+
+	/**
 	 * Reference count
 	 */
 	refcount_t ref;
@@ -73,8 +78,12 @@ bool wolfssl_rsa_fingerprint(RsaKey *rsa, cred_encoding_type_t type, chunk_t *fp
 static bool build_signature(private_wolfssl_rsa_private_key_t *this,
 							enum wc_HashType hash, chunk_t data, chunk_t *sig)
 {
-	int ret = wc_RsaSSL_Sign(data.ptr, data.len, sig->ptr, sig->len, &this->rsa,
-							 &this->rng);
+	int ret;
+
+	wc_LockMutex(&this->mutex);
+	ret = wc_RsaSSL_Sign(data.ptr, data.len, sig->ptr, sig->len, &this->rsa,
+						 &this->rng);
+	wc_UnLockMutex(&this->mutex);
 	if (ret > 0)
 	{
 		sig->len = ret;
@@ -146,8 +155,10 @@ static bool build_emsa_pss_signature(private_wolfssl_rsa_private_key_t *this,
 
 	if (wolfssl_hash_chunk(hash, data, &dgst))
 	{
+		wc_LockMutex(&this->mutex);
 		ret = wc_RsaPSS_Sign_ex(dgst.ptr, dgst.len, sig->ptr, sig->len, hash,
 								mgf, params->salt_len, &this->rsa, &this->rng);
+		wc_UnLockMutex(&this->mutex);
 		if (ret > 0)
 		{
 			sig->len = ret;
@@ -305,9 +316,11 @@ METHOD(private_key_t, decrypt, bool,
 	}
 	len = wc_RsaEncryptSize(&this->rsa);
 	*plain = chunk_alloc(len);
+	wc_LockMutex(&this->mutex);
 	len = wc_RsaPrivateDecrypt_ex(crypto.ptr, crypto.len, plain->ptr, len,
 								  &this->rsa, padding, hash, mgf,
 								  label.ptr, label.len);
+	wc_UnLockMutex(&this->mutex);
 	if (len < 0)
 	{
 		DBG1(DBG_LIB, "RSA decryption failed");
@@ -401,6 +414,7 @@ METHOD(private_key_t, destroy, void,
 	{
 		lib->encoding->clear_cache(lib->encoding, &this->rsa);
 		wc_FreeRsaKey(&this->rsa);
+		wc_FreeMutex(&this->mutex);
 		wc_FreeRng(&this->rng);
 		free(this);
 	}
@@ -439,15 +453,27 @@ static private_wolfssl_rsa_private_key_t *create_empty()
 		free(this);
 		return NULL;
 	}
+	if (wc_InitMutex(&this->mutex) != 0)
+	{
+		DBG1(DBG_LIB, "init mutex failed, rsa public key load failed");
+		wc_FreeRng(&this->rng);
+		free(this);
+		return NULL;
+	}
 	if (wc_InitRsaKey(&this->rsa, NULL) != 0)
 	{
 		DBG1(DBG_LIB, "init RSA failed, rsa private key create failed");
+		wc_FreeMutex(&this->mutex);
 		wc_FreeRng(&this->rng);
 		free(this);
 		return NULL;
 	}
 #ifdef WC_RSA_BLINDING
-	this->rsa.rng = &this->rng;
+	if (wc_RsaSetRNG(&this->rsa, &this->rng) != 0)
+	{
+		destroy(this);
+		return NULL;
+	}
 #endif
 
 	return this;

src/libstrongswan/plugins/wolfssl/wolfssl_rsa_public_key.c
@@ -58,6 +58,11 @@ struct private_wolfssl_rsa_public_key_t {
 	WC_RNG rng;
 
 	/**
+	 * Mutex to protect access to the RNG via API calls.
+	 */
+	wolfSSL_Mutex mutex;
+
+	/**
 	 * Reference counter
 	 */
 	refcount_t ref;
@@ -81,7 +86,9 @@ static bool verify_signature(private_wolfssl_rsa_public_key_t *this,
 
 	padded = chunk_copy_pad(chunk_alloca(len), signature, 0x00);
 
+	wc_LockMutex(&this->mutex);
 	len = wc_RsaSSL_VerifyInline(padded.ptr, len, &p, &this->rsa);
+	wc_UnLockMutex(&this->mutex);
 	if (len > 0)
 	{
 		success = chunk_equals_const(data, chunk_create(p, len));
@@ -149,8 +156,10 @@ static bool verify_emsa_pss_signature(private_wolfssl_rsa_public_key_t *this,
 	}
 	padded = chunk_copy_pad(chunk_alloca(len), signature, 0x00);
 
+	wc_LockMutex(&this->mutex);
 	len = wc_RsaPSS_VerifyInline_ex(padded.ptr, len, &p, hash, mgf,
 									params->salt_len, &this->rsa);
+	wc_UnLockMutex(&this->mutex);
 	if (len > 0)
 	{
 		success = wc_RsaPSS_CheckPadding_ex(dgst.ptr, dgst.len, p, len, hash,
@@ -280,9 +289,11 @@ METHOD(public_key_t, encrypt_, bool,
 	}
 	len = wc_RsaEncryptSize(&this->rsa);
 	*crypto = chunk_alloc(len);
+	wc_LockMutex(&this->mutex);
 	len = wc_RsaPublicEncrypt_ex(plain.ptr, plain.len, crypto->ptr, len,
 								 &this->rsa, &this->rng, padding, hash, mgf,
 								 label.ptr, label.len);
+	wc_UnLockMutex(&this->mutex);
 	if (len < 0)
 	{
 		DBG1(DBG_LIB, "RSA encryption failed");
@@ -423,6 +434,7 @@ METHOD(public_key_t, destroy, void,
 	{
 		lib->encoding->clear_cache(lib->encoding, &this->rsa);
 		wc_FreeRsaKey(&this->rsa);
+		wc_FreeMutex(&this->mutex);
 		wc_FreeRng(&this->rng);
 		free(this);
 	}
@@ -459,13 +471,28 @@ static private_wolfssl_rsa_public_key_t *create_empty()
 		free(this);
 		return NULL;
 	}
-	if (wc_InitRsaKey(&this->rsa, NULL) != 0)
+	if (wc_InitMutex(&this->mutex) != 0)
 	{
-		DBG1(DBG_LIB, "init RSA failed, rsa public key load failed");
+		DBG1(DBG_LIB, "init mutex failed, rsa public key load failed");
 		wc_FreeRng(&this->rng);
 		free(this);
 		return NULL;
 	}
+	if (wc_InitRsaKey(&this->rsa, NULL) != 0)
+	{
+		DBG1(DBG_LIB, "init RSA failed, rsa public key load failed");
+		wc_FreeMutex(&this->mutex);
+		wc_FreeRng(&this->rng);
+		free(this);
+		return NULL;
+	}
+#ifdef WC_RSA_BLINDING
+	if (wc_RsaSetRNG(&this->rsa, &this->rng) != 0)
+	{
+		destroy(this);
+		return NULL;
+	}
+#endif
 	return this;
 }
 