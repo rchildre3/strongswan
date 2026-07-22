rchildre3/strongswan

commit 7d7beaa1fabf8d4bc4a44b9c0ab8cb431202e3d3

Use certificate CRLIssuer information to look up cacched CRLs or CDPs

src/libstrongswan/plugins/revocation/revocation_validator.c
@@ -350,7 +350,7 @@ static bool verify_crl(certificate_t *crl)
  * Get the better of two CRLs, and check for usable CRL info
  */
 static certificate_t *get_better_crl(certificate_t *cand, certificate_t *best,
-		x509_t *subject, x509_t *issuer, cert_validation_t *valid, bool cache)
+						x509_t *subject, cert_validation_t *valid, bool cache)
 {
 	enumerator_t *enumerator;
 	time_t revocation, valid_until;
@@ -411,78 +411,116 @@ static certificate_t *get_better_crl(certificate_t *cand, certificate_t *best,
 }
 
 /**
+ * Find or fetch a certificate for a given crlIssuer
+ */
+static cert_validation_t find_crl(x509_t *subject, identification_t *issuer,
+								  certificate_t **best, bool *uri_found)
+{
+	cert_validation_t valid = VALIDATION_SKIPPED;
+	enumerator_t *enumerator;
+	certificate_t *current;
+	char *uri;
+
+	/* find a cached crl */
+	enumerator = lib->credmgr->create_cert_enumerator(lib->credmgr,
+										CERT_X509_CRL, KEY_ANY, issuer, FALSE);
+	while (enumerator->enumerate(enumerator, &current))
+	{
+		current->get_ref(current);
+		*best = get_better_crl(current, *best, subject, &valid, FALSE);
+		if (*best && valid != VALIDATION_STALE)
+		{
+			DBG1(DBG_CFG, "  using cached crl");
+			break;
+		}
+	}
+	enumerator->destroy(enumerator);
+
+	/* fallback to fetching crls from credential sets cdps */
+	if (valid != VALIDATION_GOOD && valid != VALIDATION_REVOKED)
+	{
+		enumerator = lib->credmgr->create_cdp_enumerator(lib->credmgr,
+														 CERT_X509_CRL, issuer);
+		while (enumerator->enumerate(enumerator, &uri))
+		{
+			*uri_found = TRUE;
+			current = fetch_crl(uri);
+			if (!current->has_issuer(current, issuer))
+			{
+				DBG1(DBG_CFG, "issuer of fetched CRL '%Y' does not match CRL "
+					 "issuer '%Y'", current->get_issuer(current), issuer);
+				current->destroy(current);
+				continue;
+			}
+			if (current)
+			{
+				*best = get_better_crl(current, *best, subject, &valid, TRUE);
+				if (*best && valid != VALIDATION_STALE)
+				{
+					break;
+				}
+			}
+		}
+		enumerator->destroy(enumerator);
+	}
+	return valid;
+}
+
+/**
  * validate a x509 certificate using CRL
  */
 static cert_validation_t check_crl(x509_t *subject, x509_t *issuer,
 								   auth_cfg_t *auth)
 {
 	cert_validation_t valid = VALIDATION_SKIPPED;
-	identification_t *keyid = NULL;
+	identification_t *id;
 	certificate_t *best = NULL;
+	bool uri_found = FALSE;
 	certificate_t *current;
-	public_key_t *public;
 	enumerator_t *enumerator;
 	chunk_t chunk;
-	char *uri = NULL;
+	char *uri;
 
-	/* derive the authorityKeyIdentifier from the issuer's public key */
-	current = &issuer->interface;
-	public = current->get_public_key(current);
-	if (public && public->get_fingerprint(public, KEYID_PUBKEY_SHA1, &chunk))
+	/* use issuers subjectKeyIdentifier to find a cached CRL / fetch from CDP */
+	chunk = issuer->get_subjectKeyIdentifier(issuer);
+	if (chunk.len)
 	{
-		keyid = identification_create_from_encoding(ID_KEY_ID, chunk);
-
-		/* find a cached crl by authorityKeyIdentifier */
-		enumerator = lib->credmgr->create_cert_enumerator(lib->credmgr,
-										CERT_X509_CRL, KEY_ANY, keyid, FALSE);
-		while (enumerator->enumerate(enumerator, &current))
-		{
-			current->get_ref(current);
-			best = get_better_crl(current, best, subject, issuer,
-								  &valid, FALSE);
-			if (best && valid != VALIDATION_STALE)
-			{
-				DBG1(DBG_CFG, "  using cached crl");
-				break;
-			}
-		}
-		enumerator->destroy(enumerator);
-
-		/* fallback to fetching crls from credential sets cdps */
-		if (valid != VALIDATION_GOOD && valid != VALIDATION_REVOKED)
-		{
-			enumerator = lib->credmgr->create_cdp_enumerator(lib->credmgr,
-														CERT_X509_CRL, keyid);
-			while (enumerator->enumerate(enumerator, &uri))
-			{
-				current = fetch_crl(uri);
-				if (current)
-				{
-					best = get_better_crl(current, best, subject, issuer,
-										  &valid, TRUE);
-					if (best && valid != VALIDATION_STALE)
-					{
-						break;
-					}
-				}
-			}
-			enumerator->destroy(enumerator);
-		}
-		keyid->destroy(keyid);
+		id = identification_create_from_encoding(ID_KEY_ID, chunk);
+		valid = find_crl(subject, id, &best, &uri_found);
+		id->destroy(id);
 	}
-	DESTROY_IF(public);
 
-	/* fallback to fetching crls from cdps from subject's certificate */
+	/* find a cached CRL or fetch via configured CDP via CRLIssuer */
+	enumerator = subject->create_crl_uri_enumerator(subject);
+	while (valid != VALIDATION_GOOD && valid != VALIDATION_REVOKED &&
+		   enumerator->enumerate(enumerator, &uri, &id))
+	{
+		if (id)
+		{
+			valid = find_crl(subject, id, &best, &uri_found);
+		}
+	}
+	enumerator->destroy(enumerator);
+
+	/* fallback to fetching CRLs from CDPs found in subjects certificate */
 	if (valid != VALIDATION_GOOD && valid != VALIDATION_REVOKED)
 	{
 		enumerator = subject->create_crl_uri_enumerator(subject);
-		while (enumerator->enumerate(enumerator, &uri, NULL))
+		while (enumerator->enumerate(enumerator, &uri, &id))
 		{
+			uri_found = TRUE;
 			current = fetch_crl(uri);
 			if (current)
 			{
-				best = get_better_crl(current, best, subject, issuer,
-									  &valid, TRUE);
+				if (id && !current->has_issuer(current, id))
+				{
+					DBG1(DBG_CFG, "issuer of fetched CRL '%Y' does not match "
+						 "certificates CRL issuer '%Y'",
+						 current->get_issuer(current), id);
+					current->destroy(current);
+					continue;
+				}
+				best = get_better_crl(current, best, subject, &valid, TRUE);
 				if (best && valid != VALIDATION_STALE)
 				{
 					break;
@@ -493,7 +531,7 @@ static cert_validation_t check_crl(x509_t *subject, x509_t *issuer,
 	}
 
 	/* an uri was found, but no result. switch validation state to failed */
-	if (valid == VALIDATION_SKIPPED && uri)
+	if (valid == VALIDATION_SKIPPED && uri_found)
 	{
 		valid = VALIDATION_FAILED;
 	}

commit b2f3567d4d8e5e53993dffa3a0bf765465e78416

revocation: Avoid that a skipped CRL lookup/fetch prevents fetching delta CRLs
If we find a stale CRL in the cache and finding a newer one via
CRLIssuer fails for some reason, the validation state would get
overwritten with VALIDATION_SKIPPED.  This would then prevent
fetching delta CRLs.

Fixes: 7d7beaa1fabf ("Use certificate CRLIssuer information to look up cacched CRLs or CDPs")

src/libstrongswan/plugins/revocation/revocation_validator.c
@@ -520,14 +520,14 @@ static certificate_t *get_better_crl(certificate_t *cand, certificate_t *best,
 }
 
 /**
- * Find or fetch a certificate for a given crlIssuer
+ * Find or fetch a CRL for a given subject/issuer, the validation state and
+ * best CRL are updated accordingly
  */
-static cert_validation_t find_crl(private_revocation_validator_t *this,
-								  x509_t *subject, identification_t *issuer,
-								  crl_t *base, certificate_t **best,
-								  bool *uri_found, u_int timeout)
+static void find_crl(private_revocation_validator_t *this, x509_t *subject,
+					 identification_t *issuer, crl_t *base,
+					 cert_validation_t *valid, certificate_t **best,
+					 bool *uri_found, u_int timeout)
 {
-	cert_validation_t valid = VALIDATION_SKIPPED;
 	enumerator_t *enumerator;
 	certificate_t *current;
 	char *uri;
@@ -538,8 +538,8 @@ static cert_validation_t find_crl(private_revocation_validator_t *this,
 	while (enumerator->enumerate(enumerator, &current))
 	{
 		current->get_ref(current);
-		*best = get_better_crl(current, *best, subject, &valid, FALSE, base);
-		if (*best && valid != VALIDATION_STALE)
+		*best = get_better_crl(current, *best, subject, valid, FALSE, base);
+		if (*best && *valid != VALIDATION_STALE)
 		{
 			DBG1(DBG_CFG, "  using cached crl");
 			break;
@@ -548,7 +548,7 @@ static cert_validation_t find_crl(private_revocation_validator_t *this,
 	enumerator->destroy(enumerator);
 
 	/* fallback to fetching crls from credential sets cdps */
-	if (!base && valid != VALIDATION_GOOD && valid != VALIDATION_REVOKED)
+	if (!base && *valid != VALIDATION_GOOD && *valid != VALIDATION_REVOKED)
 	{
 		enumerator = lib->credmgr->create_cdp_enumerator(lib->credmgr,
 														 CERT_X509_CRL, issuer);
@@ -565,9 +565,9 @@ static cert_validation_t find_crl(private_revocation_validator_t *this,
 					current->destroy(current);
 					continue;
 				}
-				*best = get_better_crl(current, *best, subject,
-									   &valid, TRUE, base);
-				if (*best && valid != VALIDATION_STALE)
+				*best = get_better_crl(current, *best, subject, valid,
+									   TRUE, base);
+				if (*best && *valid != VALIDATION_STALE)
 				{
 					break;
 				}
@@ -575,7 +575,6 @@ static cert_validation_t find_crl(private_revocation_validator_t *this,
 		}
 		enumerator->destroy(enumerator);
 	}
-	return valid;
 }
 
 /**
@@ -625,7 +624,7 @@ static cert_validation_t check_delta_crl(private_revocation_validator_t *this,
 	if (chunk.len)
 	{
 		id = identification_create_from_encoding(ID_KEY_ID, chunk);
-		valid = find_crl(this, subject, id, base, &best, &uri, timeout);
+		find_crl(this, subject, id, base, &valid, &best, &uri, timeout);
 		id->destroy(id);
 	}
 
@@ -636,8 +635,8 @@ static cert_validation_t check_delta_crl(private_revocation_validator_t *this,
 	{
 		if (cdp->issuer)
 		{
-			valid = find_crl(this, subject, cdp->issuer, base, &best, &uri,
-							 timeout);
+			find_crl(this, subject, cdp->issuer, base, &valid, &best, &uri,
+					 timeout);
 		}
 	}
 	enumerator->destroy(enumerator);
@@ -697,7 +696,7 @@ static cert_validation_t check_crl(private_revocation_validator_t *this,
 	if (chunk.len)
 	{
 		id = identification_create_from_encoding(ID_KEY_ID, chunk);
-		valid = find_crl(this, subject, id, NULL, &best, &uri_found, timeout);
+		find_crl(this, subject, id, NULL, &valid, &best, &uri_found, timeout);
 		id->destroy(id);
 	}
 
@@ -708,8 +707,8 @@ static cert_validation_t check_crl(private_revocation_validator_t *this,
 	{
 		if (cdp->issuer)
 		{
-			valid = find_crl(this, subject, cdp->issuer, NULL, &best,
-							 &uri_found, timeout);
+			find_crl(this, subject, cdp->issuer, NULL, &valid, &best,
+					 &uri_found, timeout);
 		}
 	}
 	enumerator->destroy(enumerator);