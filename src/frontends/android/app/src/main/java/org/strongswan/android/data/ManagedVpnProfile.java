rchildre3/strongswan

commit c2007d5b09a3d6f18d1c5e0495555735a631a71d

android: Add managed_configuration.xml
Add managed configuration and associated English strings.

src/frontends/android/app/src/main/res/xml/managed_configuration.xml
@@ -1,0 +1,296 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+    Copyright (C) 2023 Relution GmbH
+
+    Copyright (C) secunet Security Networks AG
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+-->
+<restrictions xmlns:android="http://schemas.android.com/apk/res/android">
+
+    <restriction
+        android:defaultValue="false"
+        android:description="@string/managed_config_allow_profile_creation_description"
+        android:key="allow_profile_create"
+        android:restrictionType="bool"
+        android:title="@string/managed_config_allow_profile_creation_title" />
+
+    <restriction
+        android:defaultValue="false"
+        android:description="@string/managed_config_allow_profile_import_description"
+        android:key="allow_profile_import"
+        android:restrictionType="bool"
+        android:title="@string/managed_config_allow_profile_import_title" />
+
+    <restriction
+        android:defaultValue="false"
+        android:description="@string/managed_config_allow_existing_profiles_description"
+        android:key="allow_existing_profiles"
+        android:restrictionType="bool"
+        android:title="@string/managed_config_allow_existing_profiles_title" />
+
+    <restriction
+        android:defaultValue="false"
+        android:description="@string/managed_config_allow_certificate_import_description"
+        android:key="allow_certificate_import"
+        android:restrictionType="bool"
+        android:title="@string/managed_config_allow_certificate_import_title" />
+
+    <restriction
+        android:defaultValue="false"
+        android:description="@string/managed_config_allow_settings_access_description"
+        android:key="allow_settings_access"
+        android:restrictionType="bool"
+        android:title="@string/managed_config_allow_settings_access_title" />
+
+    <restriction
+        android:defaultValue=""
+        android:description="@string/managed_config_default_vpn_profile_description"
+        android:key="pref_default_vpn_profile"
+        android:restrictionType="string"
+        android:title="@string/managed_config_default_vpn_profile_title" />
+
+    <restriction
+        android:defaultValue="false"
+        android:description="@string/managed_config_ignore_battery_optimizations_description"
+        android:key="pref_ignore_power_whitelist"
+        android:restrictionType="bool"
+        android:title="@string/managed_config_ignore_battery_optimizations_title" />
+
+    <restriction
+        android:description="@string/managed_config_profiles_array_description"
+        android:key="managed_profiles"
+        android:restrictionType="bundle_array"
+        android:title="@string/managed_config_profiles_array_title">
+
+        <restriction
+            android:description="@string/managed_config_profile_bundle_description"
+            android:key="managed_profile"
+            android:restrictionType="bundle"
+            android:title="@string/managed_config_profile_bundle_title">
+
+            <restriction
+                android:defaultValue=""
+                android:description="@string/managed_config_uuid_description"
+                android:key="_uuid"
+                android:restrictionType="string"
+                android:title="@string/managed_config_uuid_title" />
+
+            <restriction
+                android:defaultValue=""
+                android:description="@string/managed_config_name_description"
+                android:key="name"
+                android:restrictionType="string"
+                android:title="@string/managed_config_name_title" />
+
+            <restriction
+                android:defaultValue="@string/managed_config_vpn_type_default_value"
+                android:description="@string/managed_config_vpn_type_description"
+                android:entries="@array/vpn_types"
+                android:entryValues="@array/managed_config_vpn_type_values"
+                android:key="vpn_type"
+                android:restrictionType="choice"
+                android:title="@string/managed_config_vpn_type_title" />
+
+            <restriction
+                android:description="@string/managed_config_remote_bundle_description"
+                android:key="remote"
+                android:restrictionType="bundle"
+                android:title="@string/managed_config_remote_bundle_title">
+
+                <restriction
+                    android:defaultValue=""
+                    android:description="@string/managed_config_remote_addr_description"
+                    android:key="gateway"
+                    android:restrictionType="string"
+                    android:title="@string/managed_config_remote_addr_title" />
+
+                <restriction
+                    android:defaultValue="500"
+                    android:description="@string/managed_config_remote_port_description"
+                    android:key="port"
+                    android:restrictionType="integer"
+                    android:title="@string/managed_config_remote_port_title" />
+
+                <restriction
+                    android:defaultValue=""
+                    android:description="@string/managed_config_remote_id_description"
+                    android:key="remote_id"
+                    android:restrictionType="string"
+                    android:title="@string/managed_config_remote_id_title" />
+
+                <restriction
+                    android:defaultValue=""
+                    android:description="@string/managed_config_remote_cert_description"
+                    android:key="certificate"
+                    android:restrictionType="string"
+                    android:title="@string/managed_config_remote_cert_title" />
+
+                <restriction
+                    android:defaultValue="false"
+                    android:description="@string/managed_config_remote_certreq_description"
+                    android:key="remote_cert_req"
+                    android:restrictionType="bool"
+                    android:title="@string/managed_config_remote_certreq_title" />
+
+                <restriction
+                    android:defaultValue="true"
+                    android:description="@string/managed_config_remote_revocation_ocsp_description"
+                    android:key="remote_revocation_ocsp"
+                    android:restrictionType="bool"
+                    android:title="@string/managed_config_remote_revocation_ocsp_title" />
+
+                <restriction
+                    android:defaultValue="true"
+                    android:description="@string/managed_config_remote_revocation_crl_description"
+                    android:key="remote_revocation_crl"
+                    android:restrictionType="bool"
+                    android:title="@string/managed_config_remote_revocation_crl_title" />
+
+                <restriction
+                    android:defaultValue="false"
+                    android:description="@string/managed_config_remote_revocation_strict_description"
+                    android:key="remote_revocation_strict"
+                    android:restrictionType="bool"
+                    android:title="@string/managed_config_remote_revocation_strict_title" />
+            </restriction>
+
+            <restriction
+                android:description="@string/managed_config_local_bundle_description"
+                android:key="local"
+                android:restrictionType="bundle"
+                android:title="@string/managed_config_local_bundle_title">
+
+                <restriction
+                    android:defaultValue=""
+                    android:description="@string/managed_config_local_eap_id_description"
+                    android:key="username"
+                    android:restrictionType="string"
+                    android:title="@string/managed_config_local_eap_id_title" />
+
+                <restriction
+                    android:defaultValue=""
+                    android:description="@string/managed_config_local_id_description"
+                    android:key="local_id"
+                    android:restrictionType="string"
+                    android:title="@string/managed_config_local_id_title" />
+
+                <restriction
+                    android:defaultValue=""
+                    android:description="@string/managed_config_local_p12_description"
+                    android:key="user_certificate"
+                    android:restrictionType="string"
+                    android:title="@string/managed_config_local_p12_title" />
+
+                <restriction
+                    android:defaultValue="false"
+                    android:description="@string/managed_config_local_rsa_pss_description"
+                    android:key="local_rsa_pss"
+                    android:restrictionType="bool"
+                    android:title="@string/managed_config_local_rsa_pss_title" />
+
+            </restriction>
+
+            <restriction
+                android:defaultValue=""
+                android:description="@string/managed_config_included_package_names_description"
+                android:key="included_apps"
+                android:restrictionType="string"
+                android:title="@string/managed_config_included_package_names_title" />
+
+            <restriction
+                android:defaultValue=""
+                android:description="@string/managed_config_excluded_package_names_description"
+                android:key="excluded_apps"
+                android:restrictionType="string"
+                android:title="@string/managed_config_excluded_package_names_title" />
+
+            <restriction
+                android:defaultValue=""
+                android:description="@string/managed_config_ike_proposal_description"
+                android:key="ike_proposal"
+                android:restrictionType="string"
+                android:title="@string/managed_config_ike_proposal_title" />
+
+            <restriction
+                android:defaultValue=""
+                android:description="@string/managed_config_esp_proposal_description"
+                android:key="esp_proposal"
+                android:restrictionType="string"
+                android:title="@string/managed_config_esp_proposal_title" />
+
+            <restriction
+                android:defaultValue="-1"
+                android:description="@string/managed_config_mtu_description"
+                android:key="mtu"
+                android:restrictionType="integer"
+                android:title="@string/managed_config_mtu_title" />
+
+            <restriction
+                android:defaultValue="-1"
+                android:description="@string/managed_config_nat_keepalive_description"
+                android:key="nat_keepalive"
+                android:restrictionType="integer"
+                android:title="@string/managed_config_nat_keepalive_title" />
+
+            <restriction
+                android:defaultValue=""
+                android:description="@string/managed_config_dns_server_host_names_description"
+                android:key="dns_servers"
+                android:restrictionType="string"
+                android:title="@string/managed_config_dns_server_host_names_title" />
+
+            <restriction
+                android:defaultValue="false"
+                android:description="@string/managed_config_ipv6_transport_description"
+                android:key="transport_ipv6"
+                android:restrictionType="bool"
+                android:title="@string/managed_config_ipv6_transport_title" />
+
+            <restriction
+                android:description="@string/managed_config_split_tunneling_bundle_description"
+                android:key="split_tunnelling"
+                android:restrictionType="bundle"
+                android:title="@string/managed_config_split_tunneling_bundle_title">
+
+                <restriction
+                    android:defaultValue=""
+                    android:description="@string/managed_config_split_tunneling_subnets_description"
+                    android:key="included_subnets"
+                    android:restrictionType="string"
+                    android:title="@string/managed_config_split_tunneling_subnets_title" />
+
+                <restriction
+                    android:defaultValue=""
+                    android:description="@string/managed_config_split_tunneling_excluded_description"
+                    android:key="excluded_subnets"
+                    android:restrictionType="string"
+                    android:title="@string/managed_config_split_tunneling_excluded_title" />
+
+                <restriction
+                    android:defaultValue="false"
+                    android:description="@string/managed_config_split_tunneling_block_ipv4_description"
+                    android:key="split_tunnelling_block_IPv4"
+                    android:restrictionType="bool"
+                    android:title="@string/managed_config_split_tunneling_block_ipv4_title" />
+
+                <restriction
+                    android:defaultValue="false"
+                    android:description="@string/managed_config_split_tunneling_block_ipv6_description"
+                    android:key="split_tunnelling_block_IPv6"
+                    android:restrictionType="bool"
+                    android:title="@string/managed_config_split_tunneling_block_ipv6_title" />
+
+            </restriction>
+
+        </restriction>
+    </restriction>
+</restrictions>

src/frontends/android/app/src/main/AndroidManifest.xml
@@ -161,9 +161,17 @@
             </intent-filter>
         </activity>
 
+        <!--
+            Managed configuration, called app restrictions for historical reasons
+            https://developer.android.com/work/managed-configurations
+        -->
+        <meta-data
+            android:name="android.content.APP_RESTRICTIONS"
+            android:resource="@xml/managed_configuration" />
+
         <service
             android:name=".logic.VpnStateService"
-            android:exported="false" >
+            android:exported="false">
         </service>
         <service
             android:name=".logic.CharonVpnService"

src/frontends/android/app/src/main/res/values/arrays.xml
@@ -24,6 +24,15 @@
         <item>IKEv2 EAP-TNC (Username/Password)</item>
     </string-array>
 
+    <string-array name="managed_config_vpn_type_values" translatable="false">
+        <item>ikev2-eap</item>
+        <item>ikev2-cert</item>
+        <item>ikev2-cert-eap</item>
+        <item>ikev2-eap-tls</item>
+        <item>ikev2-byod-eap</item>
+    </string-array>
+    <string name="managed_config_vpn_type_default_value" translatable="false">ikev2-eap</string>
+
     <!-- the order here must match the enum entries in VpnProfile.java -->
     <string-array name="apps_handling">
         <item>All applications use the VPN</item>

src/frontends/android/app/src/main/res/values/strings_managed_configuration.xml
@@ -1,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+    Copyright (C) 2023 Relution GmbH
+
+    Copyright (C) secunet Security Networks AG
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+-->
+<resources>
+
+    <!-- Managed configuration -->
+    <string name="managed_config_allow_profile_creation_title">Allow profile creation</string>
+    <string name="managed_config_allow_profile_creation_description">Specifies whether users are allowed to add their own profiles</string>
+    <string name="managed_config_allow_profile_import_title">Allow profile import</string>
+    <string name="managed_config_allow_profile_import_description">Specifies whether users are allowed to import their own profiles</string>
+    <string name="managed_config_allow_existing_profiles_title">Show existing profiles</string>
+    <string name="managed_config_allow_existing_profiles_description">Specifies whether users can continue to see and use their previously created profiles</string>
+    <string name="managed_config_allow_certificate_import_title">Allow certificate import</string>
+    <string name="managed_config_allow_certificate_import_description">Specifies whether users are allowed to import certificates</string>
+    <string name="managed_config_allow_settings_access_title">Allow modifying settings</string>
+    <string name="managed_config_allow_settings_access_description">Specifies whether users are allowed change global app settings</string>
+    <string name="managed_config_default_vpn_profile_title">@string/pref_default_vpn_profile</string>
+    <string name="managed_config_default_vpn_profile_description">Unique identifier of the VPN profile to use by default, use the value \"mru\" for the most recently used profile</string>
+    <string name="managed_config_ignore_battery_optimizations_title">@string/pref_power_whitelist_title</string>
+    <string name="managed_config_ignore_battery_optimizations_description">@string/pref_power_whitelist_summary</string>
+    <string name="managed_config_profiles_array_title">VPN profiles</string>
+    <string name="managed_config_profiles_array_description">Collection of managed VPN profiles</string>
+    <string name="managed_config_profile_bundle_title">VPN profile</string>
+    <string name="managed_config_profile_bundle_description">A managed VPN profile</string>
+
+    <!-- Managed configuration, VPN profile -->
+    <string name="managed_config_uuid_title">Unique identifier</string>
+    <string name="managed_config_uuid_description">Unique identifier of the VPN profile. Version 4 UUIDs (random-generated) are recommended</string>
+    <string name="managed_config_name_title">@string/profile_name_label</string>
+    <string name="managed_config_name_description">@string/profile_name_hint</string>
+    <string name="managed_config_vpn_type_title">@string/profile_vpn_type_label</string>
+    <string name="managed_config_vpn_type_description">The type of client authentication used by the VPN profile</string>
+    <string name="managed_config_included_package_names_title">Apps allowed to use the VPN (Optional)</string>
+    <string name="managed_config_included_package_names_description">Space-separated list of package names; all other apps will not see/use the VPN</string>
+    <string name="managed_config_excluded_package_names_title">Apps excluded from using the VPN (Optional)</string>
+    <string name="managed_config_excluded_package_names_description">Space-separated list of package names of apps excluded from using the VPN; only used of allow list is empty</string>
+    <string name="managed_config_ike_proposal_title">@string/profile_proposals_ike_label</string>
+    <string name="managed_config_ike_proposal_description">@string/profile_proposals_ike_hint</string>
+    <string name="managed_config_esp_proposal_title">@string/profile_proposals_esp_label</string>
+    <string name="managed_config_esp_proposal_description">@string/profile_proposals_esp_hint</string>
+    <string name="managed_config_mtu_title">@string/profile_mtu_label</string>
+    <string name="managed_config_mtu_description">@string/profile_mtu_hint</string>
+    <string name="managed_config_nat_keepalive_title">@string/profile_nat_keepalive_label</string>
+    <string name="managed_config_nat_keepalive_description">@string/profile_nat_keepalive_hint</string>
+    <string name="managed_config_dns_server_host_names_title">@string/profile_dns_servers_label</string>
+    <string name="managed_config_dns_server_host_names_description">@string/profile_dns_servers_hint</string>
+    <string name="managed_config_ipv6_transport_title">@string/profile_ipv6_transport_label</string>
+    <string name="managed_config_ipv6_transport_description">@string/profile_ipv6_transport_hint</string>
+
+    <!-- Managed configuration, VPN profile, remote -->
+    <string name="managed_config_remote_bundle_title">Remote</string>
+    <string name="managed_config_remote_bundle_description">Specifies information about the server</string>
+    <string name="managed_config_remote_addr_title">@string/profile_gateway_label</string>
+    <string name="managed_config_remote_addr_description">@string/profile_gateway_hint</string>
+    <string name="managed_config_remote_port_title">@string/profile_port_label</string>
+    <string name="managed_config_remote_port_description">@string/profile_port_hint</string>
+    <string name="managed_config_remote_id_title">@string/profile_remote_id_label</string>
+    <string name="managed_config_remote_id_description">@string/profile_remote_id_hint</string>
+    <string name="managed_config_remote_cert_title">CA or server certificate (Optional)</string>
+    <string name="managed_config_remote_cert_description">Base64-encoded CA or server certificate. Is imported into the app, not the system keystore. If not set, automatic CA certificate selection is enabled</string>
+    <string name="managed_config_remote_certreq_title">Send certificate requests</string>
+    <string name="managed_config_remote_certreq_description">Specifies whether to send certificate requests for all installed or selected CA certificates. Disabling this may reduce the size of the IKE_AUTH message if the server does not support fragmentation. But it only works if the server doesn\'t require certificate requests to send back the server certificate</string>
+    <string name="managed_config_remote_revocation_ocsp_title">@string/profile_use_ocsp_label</string>
+    <string name="managed_config_remote_revocation_ocsp_description">@string/profile_use_ocsp_hint</string>
+    <string name="managed_config_remote_revocation_crl_title">@string/profile_use_crl_label</string>
+    <string name="managed_config_remote_revocation_crl_description">@string/profile_use_crl_hint</string>
+    <string name="managed_config_remote_revocation_strict_title">@string/profile_strict_revocation_label</string>
+    <string name="managed_config_remote_revocation_strict_description">@string/profile_strict_revocation_hint</string>
+
+    <!-- Managed configuration, VPN profile, local -->
+    <string name="managed_config_local_bundle_title">Local</string>
+    <string name="managed_config_local_bundle_description">Specifies information about the client</string>
+    <string name="managed_config_local_eap_id_title">Identity/username for EAP authentication (Optional)</string>
+    <string name="managed_config_local_eap_id_description">If this is required (for username/password-based EAP authentication) but not configured here, the user is prompted for it. If it is set, the user is not able to change it. In both cases the user may optionally enter the password</string>
+    <string name="managed_config_local_id_title">@string/profile_local_id_label</string>
+    <string name="managed_config_local_id_description">@string/profile_local_id_hint_user</string>
+    <string name="managed_config_local_p12_title">@string/profile_user_certificate_label</string>
+    <string name="managed_config_local_p12_description">Base64-encoded PKCS#12-container with the client certificate and private key and optional certificate chain (the latter might cause warnings on older Android releases, see Android VPN client configuration for details). Not necessary for username/password-based EAP authentication or if the user already has the certificate/key installed as it may be selected while importing the profile</string>
+    <string name="managed_config_local_rsa_pss_title">@string/profile_rsa_pss_label</string>
+    <string name="managed_config_local_rsa_pss_description">@string/profile_rsa_pss_hint</string>
+
+    <!-- Managed configuration, VPN profile, split-tunneling -->
+    <string name="managed_config_split_tunneling_bundle_title">@string/profile_split_tunneling_label</string>
+    <string name="managed_config_split_tunneling_bundle_description">@string/profile_split_tunneling_intro</string>
+    <string name="managed_config_split_tunneling_subnets_title">@string/profile_included_subnets_label</string>
+    <string name="managed_config_split_tunneling_subnets_description">@string/profile_included_subnets_hint</string>
+    <string name="managed_config_split_tunneling_excluded_title">@string/profile_excluded_subnets_label</string>
+    <string name="managed_config_split_tunneling_excluded_description">@string/profile_excluded_subnets_hint</string>
+    <string name="managed_config_split_tunneling_block_ipv4_title">@string/profile_split_tunnelingv4_title</string>
+    <string name="managed_config_split_tunneling_block_ipv4_description">Specifies whether to block IPv4 traffic that\'s not destined for the VPN. Forces all IPv4 traffic via VPN (traffic that does not match the negotiated traffic selector is then just dropped). Thus this is basically equivalent to including 0.0.0.0/0 in subnets</string>
+    <string name="managed_config_split_tunneling_block_ipv6_title">@string/profile_split_tunnelingv6_title</string>
+    <string name="managed_config_split_tunneling_block_ipv6_description">Specifies whether to block IPv6 traffic that\'s not destined for the VPN. Forces all IPv6 traffic via VPN (traffic that does not match the negotiated traffic selector is then just dropped). Thus this is basically equivalent to including ::/0 in subnets</string>
+
+</resources>

commit 5309d79221843f6225bde090b8c4d7b2a69a575b

android: Fix reading split-tunneling block settings in managed configs
The key in a Bundle is apparently case-sensitive.  Also align the
spelling with the rest of the app.

Fixes: c2007d5b09a3 ("android: Add managed_configuration.xml")

src/frontends/android/app/src/main/java/org/strongswan/android/data/ManagedVpnProfile.java
@@ -41,8 +41,8 @@ public class ManagedVpnProfile extends VpnProfile
 	private static final String KEY_LOCAL_USER_CERTIFICATE_ALIAS = "user_certificate_alias";
 	private static final String KEY_LOCAL_RSA_PSS_FLAG = "local_rsa_pss";
 
-	private static final String KEY_SPLIT_TUNNELLING_BLOCK_IPV4_FLAG = "split_tunnelling_block_ipv4";
-	private static final String KEY_SPLIT_TUNNELLING_BLOCK_IPV6_FLAG = "split_tunnelling_block_ipv6";
+	private static final String KEY_SPLIT_TUNNELING_BLOCK_IPV4_FLAG = "split_tunneling_block_ipv4";
+	private static final String KEY_SPLIT_TUNNELING_BLOCK_IPV6_FLAG = "split_tunneling_block_ipv6";
 
 	private ManagedTrustedCertificate trustedCertificate;
 	private ManagedUserCertificate userCertificate;
@@ -77,8 +77,8 @@ public class ManagedVpnProfile extends VpnProfile
 		final Bundle splitTunneling = bundle.getBundle(VpnProfileDataSource.KEY_SPLIT_TUNNELING);
 		if (splitTunneling != null)
 		{
-			splitFlags = addPositiveFlag(splitFlags, splitTunneling, KEY_SPLIT_TUNNELLING_BLOCK_IPV4_FLAG, VpnProfile.SPLIT_TUNNELING_BLOCK_IPV4);
-			splitFlags = addPositiveFlag(splitFlags, splitTunneling, KEY_SPLIT_TUNNELLING_BLOCK_IPV6_FLAG, VpnProfile.SPLIT_TUNNELING_BLOCK_IPV6);
+			splitFlags = addPositiveFlag(splitFlags, splitTunneling, KEY_SPLIT_TUNNELING_BLOCK_IPV4_FLAG, VpnProfile.SPLIT_TUNNELING_BLOCK_IPV4);
+			splitFlags = addPositiveFlag(splitFlags, splitTunneling, KEY_SPLIT_TUNNELING_BLOCK_IPV6_FLAG, VpnProfile.SPLIT_TUNNELING_BLOCK_IPV6);
 
 			setExcludedSubnets(getString(splitTunneling, VpnProfileDataSource.KEY_EXCLUDED_SUBNETS));
 			setIncludedSubnets(getString(splitTunneling, VpnProfileDataSource.KEY_INCLUDED_SUBNETS));

src/frontends/android/app/src/main/res/xml/managed_configuration.xml
@@ -300,14 +300,14 @@
                 <restriction
                     android:defaultValue="false"
                     android:description="@string/managed_config_split_tunneling_block_ipv4_description"
-                    android:key="split_tunnelling_block_IPv4"
+                    android:key="split_tunneling_block_ipv4"
                     android:restrictionType="bool"
                     android:title="@string/managed_config_split_tunneling_block_ipv4_title" />
 
                 <restriction
                     android:defaultValue="false"
                     android:description="@string/managed_config_split_tunneling_block_ipv6_description"
-                    android:key="split_tunnelling_block_IPv6"
+                    android:key="split_tunneling_block_ipv6"
                     android:restrictionType="bool"
                     android:title="@string/managed_config_split_tunneling_block_ipv6_title" />
 