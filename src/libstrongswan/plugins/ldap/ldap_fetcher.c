rchildre3/strongswan

commit 552cc11b1f017ce4962fca741f567d098f768574

merged the modularization branch (credentials) back to trunk

src/libstrongswan/plugins/ldap/ldap_fetcher.c
@@ -1,0 +1,213 @@
+/*
+ * Copyright (C) 2008 Martin Willi
+ * Copyright (C) 2007 Andreas Steffen
+ * Hochschule fuer Technik Rapperswil
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * $Id$
+ */
+
+#ifndef LDAP_DEPRECATED
+#define LDAP_DEPRECATED	1
+#endif /* LDAP_DEPRECATED */
+#include <ldap.h>
+
+#include <errno.h>
+
+#include <library.h>
+#include <debug.h>
+
+#include "ldap_fetcher.h"
+
+#define DEFAULT_TIMEOUT 10
+
+typedef struct private_ldap_fetcher_t private_ldap_fetcher_t;
+
+/**
+ * Private Data of a ldap_fetcher_t object.
+ */
+struct private_ldap_fetcher_t {
+	/**
+	 * Public data
+	 */
+	ldap_fetcher_t public;
+	
+	/**
+	 * timeout to use for fetches
+	 */
+	u_int timeout;
+};
+
+/**
+ * Parses the result returned by an ldap query
+ */
+static bool parse(LDAP *ldap, LDAPMessage *result, chunk_t *response)
+{
+	LDAPMessage *entry = ldap_first_entry(ldap, result);
+	bool success = FALSE;
+
+	if (entry)
+	{
+		BerElement *ber = NULL;
+		char *attr;
+
+		attr = ldap_first_attribute(ldap, entry, &ber);
+		if (attr)
+		{
+			struct berval **values = ldap_get_values_len(ldap, entry, attr);
+
+			if (values)
+			{
+				if (values[0])
+				{
+					*response = chunk_alloc(values[0]->bv_len);
+					memcpy(response->ptr, values[0]->bv_val, response->len);
+					success = TRUE;
+				}
+				else
+				{
+					DBG1("LDAP response contains no values");
+				}
+				ldap_value_free_len(values);
+			}
+			else
+			{
+				DBG1("getting LDAP values failed: %s", 
+					 ldap_err2string(ldap_result2error(ldap, entry, 0)));
+			}
+			ldap_memfree(attr);
+		}
+		else
+		{
+			DBG1("finding LDAP attributes failed: %s",
+				 ldap_err2string(ldap_result2error(ldap, entry, 0)));
+		}
+		ber_free(ber, 0);
+	}
+	else
+	{
+		DBG1("finding first LDAP entry failed: %s",
+			 ldap_err2string(ldap_result2error(ldap, entry, 0)));
+	}
+	return success;
+}
+
+
+static status_t fetch(private_ldap_fetcher_t *this, char *url,
+					  chunk_t *result, va_list args)
+{
+	LDAP *ldap;
+	LDAPURLDesc *lurl;
+	LDAPMessage *msg;
+	int res;
+	int ldap_version = LDAP_VERSION3;
+	struct timeval timeout;
+	status_t status = FAILED;
+	
+	if (!strneq(url, "ldap", 4))
+	{
+		return NOT_SUPPORTED;
+	}
+	if (ldap_url_parse(url, &lurl) != LDAP_SUCCESS)
+	{
+		return NOT_SUPPORTED;
+	}
+	ldap = ldap_init(lurl->lud_host, lurl->lud_port);
+	if (ldap == NULL)
+	{
+		DBG1("LDAP initialization failed: %s", strerror(errno));
+		ldap_free_urldesc(lurl);
+		return FAILED;
+	}
+	
+	timeout.tv_sec = this->timeout;
+	timeout.tv_usec = 0;
+
+	ldap_set_option(ldap, LDAP_OPT_PROTOCOL_VERSION, &ldap_version);
+	ldap_set_option(ldap, LDAP_OPT_NETWORK_TIMEOUT, &timeout);
+
+	DBG1("sending LDAP request to '%s'...", url);
+
+	res = ldap_simple_bind_s(ldap, NULL, NULL);
+	if (res == LDAP_SUCCESS)
+	{
+		res = ldap_search_st(ldap, lurl->lud_dn, lurl->lud_scope,
+							 lurl->lud_filter, lurl->lud_attrs,
+							 0, &timeout, &msg);
+
+		if (res == LDAP_SUCCESS)
+		{
+			if (parse(ldap, msg, result))
+			{
+				status = SUCCESS;
+			}
+			ldap_msgfree(msg);
+		}
+		else
+		{
+			DBG1("LDAP search failed: %s", ldap_err2string(res));
+		}
+	}
+	else
+	{
+		DBG1("LDAP bind to '%s' failed: %s", url, ldap_err2string(res));
+	}
+	ldap_unbind_s(ldap);
+	ldap_free_urldesc(lurl);
+	return status;
+}
+
+
+/**
+ * Implementation of fetcher_t.set_option.
+ */
+static bool set_option(private_ldap_fetcher_t *this, fetcher_option_t option, ...)
+{
+	va_list args;
+	
+	va_start(args, option);
+	switch (option)
+	{
+		case FETCH_TIMEOUT:
+		{
+			this->timeout = va_arg(args, u_int);
+			return TRUE;
+		}
+		default:
+			return FALSE;
+	}
+}
+
+/**
+ * Implements ldap_fetcher_t.destroy
+ */
+static void destroy(private_ldap_fetcher_t *this)
+{
+	free(this);
+}
+
+/*
+ * Described in header.
+ */
+ldap_fetcher_t *ldap_fetcher_create()
+{
+	private_ldap_fetcher_t *this = malloc_thing(private_ldap_fetcher_t);
+
+	this->public.interface.fetch = (status_t(*)(fetcher_t*,char*,chunk_t*))fetch;
+	this->public.interface.set_option = (bool(*)(fetcher_t*, fetcher_option_t option, ...))set_option;
+	this->public.interface.destroy = (void (*)(fetcher_t*))destroy;
+	
+	this->timeout = DEFAULT_TIMEOUT;
+	
+	return &this->public;
+}
+

src/libstrongswan/utils/leak_detective.c
@@ -1,11 +1,5 @@
-/**
- * @file leak_detective.c
- * 
- * @brief Allocation hooks to find memory leaks.
- */
-
 /*
- * Copyright (C) 2006 Martin Willi
+ * Copyright (C) 2006-2008 Martin Willi
  * Hochschule fuer Technik Rapperswil
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -17,8 +11,15 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
+ *
+ * $Id$
  */
 
+#ifdef HAVE_DLADDR
+# define _GNU_SOURCE
+# include <dlfcn.h>
+#endif /* HAVE_DLADDR */
+	
 #include <stddef.h>
 #include <string.h>
 #include <stdio.h>
@@ -33,6 +34,7 @@
 #include <pthread.h>
 #include <netdb.h>
 #include <printf.h>
+#include <locale.h>
 #ifdef HAVE_BACKTRACE
 # include <execinfo.h>
 #endif /* HAVE_BACKTRACE */
@@ -42,7 +44,18 @@
 #include <library.h>
 #include <debug.h>
 
-#ifdef LEAK_DETECTIVE
+typedef struct private_leak_detective_t private_leak_detective_t;
+
+/**
+ * private data of leak_detective
+ */
+struct private_leak_detective_t {
+
+	/**
+	 * public functions
+	 */
+	leak_detective_t public;
+};
 
 /**
  * Magic value which helps to detect memory corruption. Yummy!
@@ -146,77 +159,103 @@ static void log_stack_frames(void **stack_frames, int stack_frame_count)
 	char **strings;
 	size_t i;
 
-	strings = backtrace_symbols (stack_frames, stack_frame_count);
+	strings = backtrace_symbols(stack_frames, stack_frame_count);
 
-	DBG1("  dumping %d stack frame addresses", stack_frame_count);
+	fprintf(stderr, " dumping %d stack frame addresses\n", stack_frame_count);
 
 	for (i = 0; i < stack_frame_count; i++)
 	{
-		DBG1("    %s", strings[i]);
+#ifdef HAVE_DLADDR
+		Dl_info info;
+		
+		/* TODO: this is quite hackish, but it works. A more proper solution
+		 * would execve addr2strongline and pipe the output to DBG1() */
+		if (dladdr(stack_frames[i], &info))
+		{
+			char cmd[1024];
+			void *ptr = stack_frames[i];
+			
+			if (strstr(info.dli_fname, ".so"))
+			{
+				ptr = (void*)(stack_frames[i] - info.dli_fbase);
+			}
+			snprintf(cmd, sizeof(cmd), "addr2line -e %s %p", info.dli_fname, ptr);
+			if (info.dli_sname)
+			{
+				fprintf(stderr, "  \e[33m%s\e[0m @ %p (\e[31m%s+0x%x\e[0m) [%p]\n",
+						info.dli_fname, info.dli_fbase, info.dli_sname,
+						stack_frames[i] - info.dli_saddr, stack_frames[i]);
+			}
+			else
+			{
+				fprintf(stderr, "  \e[33m%s\e[0m @ %p [%p]\n", info.dli_fname,
+						info.dli_fbase, stack_frames[i]);
+			}
+			fprintf(stderr, "    -> \e[32m");
+			system(cmd);
+			fprintf(stderr, "\e[0m");
+		}
+		else
+#endif /* HAVE_DLADDR */
+		{
+			fprintf(stderr, "    %s\n", strings[i]);
+		}
 	}
 	free (strings);
 #endif /* HAVE_BACKTRACE */
 }
 
 /**
- * Whitelist, which contains address ranges in stack frames ignored when leaking.
- * 
- * This is necessary, as some function use allocation hacks (static buffers)
- * and so on, which we want to suppress on leak reports.
+ * Leak report white list
  *
- * The range_size is calculated using the readelf utility, e.g.:
- * readelf -s /lib/glibc.so.6
- * The values are for glibc-2.4 and may or may not be correct on other systems.
+ * List of functions using static allocation buffers or should be suppressed
+ * otherwise on leak report. 
  */
-typedef struct whitelist_t whitelist_t;
-
-struct whitelist_t {
-	void* range_start;
-	size_t range_size;
-};
-
-#ifdef LIBCURL
-/* dummy declaration for whitelisting */
-void *Curl_getaddrinfo(void);
-#endif /* LIBCURL */
-
-whitelist_t whitelist[] = {
-	{pthread_create,			2542},
-	{pthread_setspecific,		 217},
-	{mktime,					  60},
-	{tzset,						 123},
-	{inet_ntoa,					 249},
-	{strerror,					 180},
-	{getprotobynumber,			 291},
-	{getservbyport,				 311},
-	{register_printf_function,	 159},
-	{syslog,					  44},
-	{vsyslog,					  41},
-	{dlopen,					 109},
-#	ifdef LIBCURL
-	/* from /usr/lib/libcurl.so.3 */
-	{Curl_getaddrinfo,			 480},
-#	endif /* LIBCURL */
+char *whitelist[] = {
+	"pthread_create",
+	"pthread_setspecific",
+	"mktime",
+	"tzset",
+	"inet_ntoa",
+	"strerror",
+	"getprotobynumber",
+	"getservbyport",
+	"getservbyname",
+	"register_printf_function",
+	"syslog",
+	"vsyslog",
+	"dlopen",
+	"getaddrinfo",
+	"setlocale",
+	"mysql_init_character_set",
+	"init_client_errs",
+	"my_thread_init",
 };
 
 /**
- * Check if this stack frame is whitelisted.
+ * check if a stack frame contains functions listed above
  */
 static bool is_whitelisted(void **stack_frames, int stack_frame_count)
 {
 	int i, j;
 	
+#ifdef HAVE_DLADDR
 	for (i=0; i< stack_frame_count; i++)
 	{
-		for (j=0; j<sizeof(whitelist)/sizeof(whitelist_t); j++)
-		{
-			if (stack_frames[i] >= whitelist[j].range_start &&
-				stack_frames[i] <= (whitelist[j].range_start + whitelist[j].range_size))
+		Dl_info info;
+		
+		if (dladdr(stack_frames[i], &info) && info.dli_sname)
+		{	
+			for (j = 0; j < sizeof(whitelist)/sizeof(char*); j++)
 			{
-				return TRUE;
+				if (streq(info.dli_sname, whitelist[j]))
+				{
+					return TRUE;
+				}
 			}
 		}
 	}
+#endif /* HAVE_DLADDR */
 	return FALSE;
 }
 
@@ -232,8 +271,9 @@ void report_leaks()
 	{
 		if (!is_whitelisted(hdr->stack_frames, hdr->stack_frame_count))
 		{
-			DBG1("Leak (%d bytes at %p):", hdr->bytes, hdr + 1);
-			log_stack_frames(hdr->stack_frames, hdr->stack_frame_count);
+			fprintf(stderr, "Leak (%d bytes at %p):\n", hdr->bytes, hdr + 1);
+			/* skip the first frame, contains leak detective logic */
+			log_stack_frames(hdr->stack_frames + 1, hdr->stack_frame_count - 1);
 			leaks++;
 		}
 	}
@@ -241,13 +281,13 @@ void report_leaks()
 	switch (leaks)
 	{
 		case 0:
-			DBG1("No leaks detected");
+			fprintf(stderr, "No leaks detected\n");
 			break;
 		case 1:
-			DBG1("One leak detected");
+			fprintf(stderr, "One leak detected\n");
 			break;
 		default:
-			DBG1("%d leaks detected", leaks);
+			fprintf(stderr, "%d leaks detected\n", leaks);
 			break;
 	}
 }
@@ -334,8 +374,8 @@ void free_hook(void *ptr, const void *caller)
 	uninstall_hooks();
 	if (hdr->magic != MEMORY_HEADER_MAGIC)
 	{
-		DBG1("freeing of invalid memory (%p, MAGIC 0x%x != 0x%x):",
-			 ptr, hdr->magic, MEMORY_HEADER_MAGIC);
+		fprintf(stderr, "freeing of invalid memory (%p, MAGIC 0x%x != 0x%x):\n",
+				ptr, hdr->magic, MEMORY_HEADER_MAGIC);
 		stack_frame_count = backtrace(stack_frames, STACK_FRAMES_COUNT);
 		log_stack_frames(stack_frames, stack_frame_count);
 		install_hooks();
@@ -380,7 +420,7 @@ void *realloc_hook(void *old, size_t bytes, const void *caller)
 	uninstall_hooks();
 	if (hdr->magic != MEMORY_HEADER_MAGIC)
 	{
-		DBG1("reallocation of invalid memory (%p):", old);
+		fprintf(stderr, "reallocation of invalid memory (%p):\n", old);
 		stack_frame_count = backtrace(stack_frames, STACK_FRAMES_COUNT);
 		log_stack_frames(stack_frames, stack_frame_count);
 		install_hooks();
@@ -407,65 +447,31 @@ void *realloc_hook(void *old, size_t bytes, const void *caller)
 }
 
 /**
- * Setup leak detective
+ * Implementation of leak_detective_t.destroy
  */
-void __attribute__ ((constructor)) leak_detective_init()
+static void destroy(private_leak_detective_t *this)
 {
-	if (getenv("LEAK_DETECTIVE_DISABLE") == NULL)
-	{
-		install_hooks();
-	}
-}
-
-/**
- * Clean up leak detective
- */
-void __attribute__ ((destructor)) leak_detective_cleanup()
-{
-	if (getenv("LEAK_DETECTIVE_DISABLE") == NULL)
+	if (installed)
 	{
 		uninstall_hooks();
 		report_leaks();
 	}
+	free(this);
 }
 
-/**
- * Log memory allocation statistics
+/*
+ * see header file
  */
-void leak_detective_status(FILE *stream)
+leak_detective_t *leak_detective_create()
 {
-	u_int blocks = 0;
-	size_t bytes = 0;
-	memory_header_t *hdr = &first_header;
+	private_leak_detective_t *this = malloc_thing(private_leak_detective_t);
 	
-	if (getenv("LEAK_DETECTIVE_DISABLE"))
+	this->public.destroy = (void(*)(leak_detective_t*))destroy;
+	
+	if (getenv("LEAK_DETECTIVE_DISABLE") == NULL)
 	{
-		return;
+		install_hooks();
 	}
-	
-	pthread_mutex_lock(&mutex);
-	while ((hdr = hdr->next))
-	{
-		blocks++;
-		bytes += hdr->bytes;
-	}
-	pthread_mutex_unlock(&mutex);
-	
-	fprintf(stream, "allocation statistics:\n");
-	fprintf(stream, "  call stats: malloc: %d, free: %d, realloc: %d\n",
-			count_malloc, count_free, count_realloc);
-	fprintf(stream, "  allocated %d blocks, total size %d bytes (avg. %d bytes)\n",
-			blocks, bytes, bytes/blocks);
+	return &this->public;
 }
 
-#else /* !LEAK_DETECTION */
-
-/**
- * Dummy when !using LEAK_DETECTIVE
- */
-void leak_detective_status(FILE *stream)
-{
-
-}
-
-#endif /* LEAK_DETECTION */

commit 551d8c3f081c03aa4074e1e8ec7fbb8377c1233d

ldap: Replace deprecated function calls and support LDAPS
The use of the deprecated `ldap_init()` meant that LDAPS, although
announced by the plugin, was not actually supported.  The plugin just
always used a plaintext connection.  Now we use the current API and
get support for LDAPS (requires a bit of an awkward URI construction).
Based on the URI's scheme we also set an option to enforce a certificate
check.  The new NEWCTX option creates a connection-specific TLS context.
Without that we get a global default context once bind is called that is
not freed until the daemon exits (it leaks in LD and also seems unsafe
in regards to multiple threads fetching CRLs via LDAP).

Fixes: 552cc11b1f01 ("merged the modularization branch (credentials) back to trunk")
Fixes: 8c06e9c0edc5 ("added #define LDAP_DEPRECATED in order to use old ldap_init() function")

configure.ac
@@ -1027,7 +1027,7 @@ if test x$gmp = xtrue; then
 fi
 
 if test x$ldap = xtrue; then
-	AC_CHECK_LIB([ldap],[ldap_init],[LIBS="$LIBS"],[AC_MSG_ERROR([LDAP library ldap not found])],[])
+	AC_CHECK_LIB([ldap],[ldap_initialize],[LIBS="$LIBS"],[AC_MSG_ERROR([LDAP library ldap not found])],[])
 	AC_CHECK_LIB([lber],[ber_free],[LIBS="$LIBS"],[AC_MSG_ERROR([LDAP library lber not found])],[])
 	AC_CHECK_HEADER([ldap.h],,[AC_MSG_ERROR([LDAP header ldap.h not found!])])
 fi

src/libstrongswan/plugins/ldap/ldap_fetcher.c
@@ -15,12 +15,9 @@
  * for more details.
  */
 
-#ifndef LDAP_DEPRECATED
-#define LDAP_DEPRECATED	1
-#endif /* LDAP_DEPRECATED */
-#include <ldap.h>
+#define _GNU_SOURCE /* for asprintf() */
 
-#include <errno.h>
+#include <ldap.h>
 
 #include <library.h>
 #include <utils/debug.h>
@@ -53,6 +50,7 @@ static bool parse(LDAP *ldap, LDAPMessage *result, chunk_t *response)
 {
 	LDAPMessage *entry = ldap_first_entry(ldap, result);
 	bool success = FALSE;
+	int res = 0;
 
 	if (entry)
 	{
@@ -80,15 +78,17 @@ static bool parse(LDAP *ldap, LDAPMessage *result, chunk_t *response)
 			}
 			else
 			{
+				ldap_get_option(ldap, LDAP_OPT_RESULT_CODE, &res);
 				DBG1(DBG_LIB, "getting LDAP values failed: %s",
-					 ldap_err2string(ldap_result2error(ldap, entry, 0)));
+					 ldap_err2string(res));
 			}
 			ldap_memfree(attr);
 		}
 		else
 		{
+			ldap_get_option(ldap, LDAP_OPT_RESULT_CODE, &res);
 			DBG1(DBG_LIB, "finding LDAP attributes failed: %s",
-				 ldap_err2string(ldap_result2error(ldap, entry, 0)));
+				 ldap_err2string(res));
 		}
 		ber_free(ber, 0);
 	}
@@ -99,18 +99,72 @@ static bool parse(LDAP *ldap, LDAPMessage *result, chunk_t *response)
 	return success;
 }
 
+/**
+ * Set an LDAP option and report the error if necessary
+ */
+static bool set_ldap_option(LDAP *ldap, int option, const void *value,
+							const char *name)
+{
+	int res;
+
+	res = ldap_set_option(ldap, option, value);
+	if (res != LDAP_OPT_SUCCESS)
+	{
+		DBG1(DBG_LIB, "setting LDAP option '%s' failed: %s", name,
+			 ldap_err2string(res));
+		return FALSE;
+	}
+	return TRUE;
+}
+
+/**
+ * Generate an URL accepted by ldap_initialize(), only consisting of scheme,
+ * host and port.
+ */
+static char *build_ldap_url(LDAPURLDesc *lurl)
+{
+	char *url = NULL;
+	bool ipv6;
+
+	if (!lurl->lud_host || !*lurl->lud_host)
+	{
+		DBG1(DBG_LIB, "LDAP URL without host not supported");
+		return NULL;
+	}
+
+	ipv6 = strchr(lurl->lud_host, ':') != NULL;
+	if (lurl->lud_port)
+	{
+		if (asprintf(&url, ipv6 ? "%s://[%s]:%d" : "%s://%s:%d",
+					 lurl->lud_scheme, lurl->lud_host, lurl->lud_port) < 0)
+		{
+			return NULL;
+		}
+	}
+	else
+	{
+		if (asprintf(&url, ipv6 ? "%s://[%s]" : "%s://%s", lurl->lud_scheme,
+					 lurl->lud_host) < 0)
+		{
+			return NULL;
+		}
+	}
+	return url;
+}
 
 METHOD(fetcher_t, fetch, status_t,
 	private_ldap_fetcher_t *this, char *url, void *userdata)
 {
-	LDAP *ldap;
+	LDAP *ldap = NULL;
 	LDAPURLDesc *lurl;
+	char *ldap_url;
 	LDAPMessage *msg;
-	int res;
-	int ldap_version = LDAP_VERSION3;
-	struct timeval timeout;
-	status_t status = FAILED;
 	chunk_t *result = userdata;
+	struct timeval timeout;
+	int ldap_version = LDAP_VERSION3;
+	int res;
+	struct berval cred = {0, ""};
+	status_t status = FAILED;
 
 	if (!strpfx(url, "ldap"))
 	{
@@ -120,29 +174,65 @@ METHOD(fetcher_t, fetch, status_t,
 	{
 		return NOT_SUPPORTED;
 	}
-	ldap = ldap_init(lurl->lud_host, lurl->lud_port);
-	if (ldap == NULL)
+	if (!streq(lurl->lud_scheme, "ldap") && !streq(lurl->lud_scheme, "ldaps"))
 	{
-		DBG1(DBG_LIB, "LDAP initialization failed: %s", strerror(errno));
+		ldap_free_urldesc(lurl);
+		return NOT_SUPPORTED;
+	}
+	/* ldap_initialize() only expects "scheme://host:port", so we have to
+	 * generate an appropriate string */
+	ldap_url = build_ldap_url(lurl);
+	if (!ldap_url)
+	{
 		ldap_free_urldesc(lurl);
 		return FAILED;
+	}
+	res = ldap_initialize(&ldap, ldap_url);
+	free(ldap_url);
+	if (res != LDAP_SUCCESS)
+	{
+		DBG1(DBG_LIB, "LDAP initialization failed: %s", ldap_err2string(res));
+		ldap_free_urldesc(lurl);
+		return FAILED;
+	}
+
+	if (streq(lurl->lud_scheme, "ldaps"))
+	{
+		int require_cert = LDAP_OPT_X_TLS_DEMAND;
+
+		if (!set_ldap_option(ldap, LDAP_OPT_X_TLS_REQUIRE_CERT, &require_cert,
+							 "TLS require cert") ||
+			!set_ldap_option(ldap, LDAP_OPT_X_TLS_NEWCTX, LDAP_OPT_ON,
+							 "TLS context"))
+		{
+			ldap_unbind_ext_s(ldap, NULL, NULL);
+			ldap_free_urldesc(lurl);
+			return FAILED;
+		}
 	}
 
 	timeout.tv_sec = this->timeout;
 	timeout.tv_usec = 0;
 
-	ldap_set_option(ldap, LDAP_OPT_PROTOCOL_VERSION, &ldap_version);
-	ldap_set_option(ldap, LDAP_OPT_NETWORK_TIMEOUT, &timeout);
-	ldap_set_option(ldap, LDAP_OPT_TIMEOUT, &timeout);
+	if (!set_ldap_option(ldap, LDAP_OPT_PROTOCOL_VERSION, &ldap_version,
+						 "protocol version") ||
+		!set_ldap_option(ldap, LDAP_OPT_NETWORK_TIMEOUT, &timeout,
+						 "network timeout") ||
+		!set_ldap_option(ldap, LDAP_OPT_TIMEOUT, &timeout, "timeout"))
+	{
+		ldap_unbind_ext_s(ldap, NULL, NULL);
+		ldap_free_urldesc(lurl);
+		return FAILED;
+	}
 
 	DBG2(DBG_LIB, "sending LDAP request to '%s'...", url);
 
-	res = ldap_simple_bind_s(ldap, NULL, NULL);
+	res = ldap_sasl_bind_s(ldap, NULL, LDAP_SASL_SIMPLE, &cred, NULL, NULL, NULL);
 	if (res == LDAP_SUCCESS)
 	{
-		res = ldap_search_st(ldap, lurl->lud_dn, lurl->lud_scope,
-							 lurl->lud_filter, lurl->lud_attrs,
-							 0, &timeout, &msg);
+		res = ldap_search_ext_s(ldap, lurl->lud_dn, lurl->lud_scope,
+								lurl->lud_filter, lurl->lud_attrs,
+								0, NULL, NULL, &timeout, LDAP_NO_LIMIT, &msg);
 
 		if (res == LDAP_SUCCESS)
 		{
@@ -162,7 +252,7 @@ METHOD(fetcher_t, fetch, status_t,
 		DBG1(DBG_LIB, "LDAP bind to '%s' failed: %s", url,
 			 ldap_err2string(res));
 	}
-	ldap_unbind_s(ldap);
+	ldap_unbind_ext_s(ldap, NULL, NULL);
 	ldap_free_urldesc(lurl);
 	return status;
 }
@@ -213,4 +303,3 @@ ldap_fetcher_t *ldap_fetcher_create()
 
 	return &this->public;
 }
-

src/libstrongswan/utils/leak_detective.c
@@ -565,6 +565,7 @@ static char *whitelist[] = {
 	"Curl_client_write",
 	/* libldap */
 	"ldap_int_initialize",
+	"ldap_pvt_tls_set_option",
 	/* ClearSilver */
 	"nerr_init",
 	/* libgcrypt */