rchildre3/strongswan

commit c92eade82c814e9f6b55f80d715bb51d28d5e1fb

wolfssl: Add wolfSSL plugin for cryptographic implementations

src/libstrongswan/plugins/wolfssl/wolfssl_aead.c
@@ -1,0 +1,484 @@
+/*
+ * Copyright (C) 2019 Sean Parkinson, wolfSSL Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "wolfssl_common.h"
+
+#if (!defined(NO_AES) && (defined(HAVE_AESGCM) || defined(HAVE_AESCCM))) || \
+								(defined(HAVE_CHACHA) && defined(HAVE_POLY1305))
+
+#include "wolfssl_aead.h"
+
+#include <wolfssl/wolfcrypt/aes.h>
+#include <wolfssl/wolfcrypt/chacha.h>
+#include <wolfssl/wolfcrypt/chacha20_poly1305.h>
+#include <crypto/iv/iv_gen_seq.h>
+
+/** as defined in RFC 4106 */
+#define IV_LEN			8
+#define GCM_SALT_LEN	4
+#define GCM_NONCE_LEN	(GCM_SALT_LEN + IV_LEN)
+
+#define CCM_SALT_LEN	3
+#define CCM_NONCE_LEN	(CCM_SALT_LEN + IV_LEN)
+
+#if !defined(NO_AES) && defined(HAVE_AESGCM)
+#define MAX_NONCE_LEN	GCM_NONCE_LEN
+#define MAX_SALT_LEN	GCM_SALT_LEN
+#elif defined(HAVE_CHACHA) && defined(HAVE_POLY1305)
+#define MAX_NONCE_LEN	12
+#define MAX_SALT_LEN	4
+#elif !defined(NO_AES) && defined(HAVE_AESCCM)
+#define MAX_NONCE_LEN	CCM_NONCE_LEN
+#define MAX_SALT_LEN	GCM_SALT_LEN
+#endif
+
+typedef struct private_aead_t private_aead_t;
+
+/**
+ * Private data of aead_t
+ */
+struct private_aead_t {
+
+	/**
+	 * Public interface
+	 */
+	aead_t public;
+
+	/**
+	 * The encryption key
+	 */
+	chunk_t	key;
+
+	/**
+	 * Salt value
+	 */
+	char salt[MAX_SALT_LEN];
+
+	/**
+	 * Length of the salt
+	 */
+	size_t salt_len;
+
+	/**
+	 * Size of the integrity check value
+	 */
+	size_t icv_size;
+
+	/**
+	 * Size of the IV
+	 */
+	size_t iv_size;
+
+	/**
+	 * IV generator
+	 */
+	iv_gen_t *iv_gen;
+
+	/**
+	 * The cipher to use
+	 */
+	union
+	{
+#if !defined(NO_AES) && (defined(HAVE_AESGCM) || defined(HAVE_AESCCM))
+		Aes aes;
+#endif
+	} cipher;
+
+	/**
+	 * The cipher to use
+	 */
+	encryption_algorithm_t alg;
+};
+
+
+METHOD(aead_t, encrypt, bool,
+	private_aead_t *this, chunk_t plain, chunk_t assoc, chunk_t iv,
+	chunk_t *encrypted)
+{
+	bool success = FALSE;
+	int ret = 0;
+	u_char *out;
+	u_char nonce[MAX_NONCE_LEN];
+
+	out = plain.ptr;
+	if (encrypted)
+	{
+		*encrypted = chunk_alloc(plain.len + this->icv_size);
+		out = encrypted->ptr;
+	}
+
+	memcpy(nonce, this->salt, this->salt_len);
+	memcpy(nonce + this->salt_len, iv.ptr, IV_LEN);
+
+	switch (this->alg)
+	{
+#if !defined(NO_AES) && defined(HAVE_AESGCM)
+		case ENCR_AES_GCM_ICV8:
+		case ENCR_AES_GCM_ICV12:
+		case ENCR_AES_GCM_ICV16:
+			ret = wc_AesGcmSetKey(&this->cipher.aes, this->key.ptr,
+								  this->key.len);
+			if (ret == 0)
+			{
+				ret = wc_AesGcmEncrypt(&this->cipher.aes, out, plain.ptr,
+					plain.len, nonce, GCM_NONCE_LEN, out + plain.len,
+					this->icv_size, assoc.ptr, assoc.len);
+			}
+			success = (ret == 0);
+			break;
+#endif
+#if !defined(NO_AES) && defined(HAVE_AESCCM)
+		case ENCR_AES_CCM_ICV8:
+		case ENCR_AES_CCM_ICV12:
+		case ENCR_AES_CCM_ICV16:
+			if (plain.ptr == NULL && plain.len == 0)
+				plain.ptr = nonce;
+			ret = wc_AesCcmSetKey(&this->cipher.aes, this->key.ptr,
+								  this->key.len);
+			if (ret == 0)
+			{
+				ret = wc_AesCcmEncrypt(&this->cipher.aes, out, plain.ptr,
+					plain.len, nonce, CCM_NONCE_LEN, out + plain.len,
+					this->icv_size, assoc.ptr, assoc.len);
+			}
+			success = (ret == 0);
+			break;
+#endif
+#if defined(HAVE_CHACHA) && defined(HAVE_POLY1305)
+		case ENCR_CHACHA20_POLY1305:
+			ret = wc_ChaCha20Poly1305_Encrypt(this->key.ptr, nonce, assoc.ptr,
+					assoc.len, plain.ptr, plain.len, out, out + plain.len);
+			success = (ret == 0);
+			break;
+#endif
+		default:
+			break;
+	}
+
+	return success;
+}
+
+METHOD(aead_t, decrypt, bool,
+	private_aead_t *this, chunk_t encrypted, chunk_t assoc, chunk_t iv,
+	chunk_t *plain)
+{
+	bool success = FALSE;
+	int ret = 0;
+	u_char *out;
+	u_char nonce[MAX_NONCE_LEN];
+
+	if (encrypted.len < this->icv_size)
+	{
+		return FALSE;
+	}
+	encrypted.len -= this->icv_size;
+
+	out = encrypted.ptr;
+	if (plain)
+	{
+		*plain = chunk_alloc(encrypted.len);
+		out = plain->ptr;
+	}
+
+	memcpy(nonce, this->salt, this->salt_len);
+	memcpy(nonce + this->salt_len, iv.ptr, IV_LEN);
+
+	switch (this->alg)
+	{
+#if !defined(NO_AES) && defined(HAVE_AESGCM)
+		case ENCR_AES_GCM_ICV8:
+		case ENCR_AES_GCM_ICV12:
+		case ENCR_AES_GCM_ICV16:
+			ret = wc_AesGcmSetKey(&this->cipher.aes, this->key.ptr,
+				  this->key.len);
+			if (ret == 0)
+			{
+				ret = wc_AesGcmDecrypt(&this->cipher.aes, out, encrypted.ptr,
+					encrypted.len, nonce, GCM_NONCE_LEN,
+					encrypted.ptr + encrypted.len, this->icv_size, assoc.ptr,
+					assoc.len);
+			}
+			success = (ret == 0);
+			break;
+#endif
+#if !defined(NO_AES) && defined(HAVE_AESCCM)
+		case ENCR_AES_CCM_ICV8:
+		case ENCR_AES_CCM_ICV12:
+		case ENCR_AES_CCM_ICV16:
+			if (encrypted.ptr == NULL && encrypted.len == 0)
+				encrypted.ptr = nonce;
+			if (out == NULL && encrypted.len == 0)
+				out = nonce;
+			ret = wc_AesCcmSetKey(&this->cipher.aes, this->key.ptr,
+				  this->key.len);
+			if (ret == 0)
+			{
+				ret = wc_AesCcmDecrypt(&this->cipher.aes, out, encrypted.ptr,
+					encrypted.len, nonce, CCM_NONCE_LEN,
+					encrypted.ptr + encrypted.len, this->icv_size, assoc.ptr,
+					assoc.len);
+			}
+			success = (ret == 0);
+			break;
+#endif
+#if defined(HAVE_CHACHA) && defined(HAVE_POLY1305)
+		case ENCR_CHACHA20_POLY1305:
+			ret = wc_ChaCha20Poly1305_Decrypt(this->key.ptr, nonce, assoc.ptr,
+					assoc.len, encrypted.ptr, encrypted.len,
+					encrypted.ptr + encrypted.len, out);
+			success = (ret == 0);
+			break;
+#endif
+		default:
+			break;
+	}
+
+	return success;
+}
+
+METHOD(aead_t, get_block_size, size_t,
+	private_aead_t *this)
+{
+	/* All AEAD algorithms are streaming. */
+	return 1;
+}
+
+METHOD(aead_t, get_icv_size, size_t,
+	private_aead_t *this)
+{
+	return this->icv_size;
+}
+
+METHOD(aead_t, get_iv_size, size_t,
+	private_aead_t *this)
+{
+	return IV_LEN;
+}
+
+METHOD(aead_t, get_iv_gen, iv_gen_t*,
+	private_aead_t *this)
+{
+	return this->iv_gen;
+}
+
+METHOD(aead_t, get_key_size, size_t,
+	private_aead_t *this)
+{
+	return this->key.len + this->salt_len;
+}
+
+METHOD(aead_t, set_key, bool,
+	private_aead_t *this, chunk_t key)
+{
+	if (key.len != get_key_size(this))
+	{
+		return FALSE;
+	}
+	memcpy(this->salt, key.ptr + key.len - this->salt_len, this->salt_len);
+	memcpy(this->key.ptr, key.ptr, this->key.len);
+	return TRUE;
+}
+
+METHOD(aead_t, destroy, void,
+	private_aead_t *this)
+{
+	chunk_clear(&this->key);
+	switch (this->alg)
+	{
+#if !defined(NO_AES) && defined(HAVE_AESGCM)
+		case ENCR_AES_GCM_ICV8:
+		case ENCR_AES_GCM_ICV12:
+		case ENCR_AES_GCM_ICV16:
+			wc_AesFree(&this->cipher.aes);
+			break;
+#endif
+#if !defined(NO_AES) && defined(HAVE_AESCCM)
+		case ENCR_AES_CCM_ICV8:
+		case ENCR_AES_CCM_ICV12:
+		case ENCR_AES_CCM_ICV16:
+			wc_AesFree(&this->cipher.aes);
+			break;
+#endif
+#if defined(HAVE_CHACHA) && defined(HAVE_POLY1305)
+		case ENCR_CHACHA20_POLY1305:
+			break;
+#endif
+		default:
+			break;
+	}
+	this->iv_gen->destroy(this->iv_gen);
+	free(this);
+}
+
+/*
+ * Described in header
+ */
+aead_t *wolfssl_aead_create(encryption_algorithm_t algo,
+							size_t key_size, size_t salt_size)
+{
+	private_aead_t *this;
+
+	INIT(this,
+		.public = {
+			.encrypt = _encrypt,
+			.decrypt = _decrypt,
+			.get_block_size = _get_block_size,
+			.get_icv_size = _get_icv_size,
+			.get_iv_size = _get_iv_size,
+			.get_iv_gen = _get_iv_gen,
+			.get_key_size = _get_key_size,
+			.set_key = _set_key,
+			.destroy = _destroy,
+		},
+		.alg = algo,
+	);
+
+	switch (algo)
+	{
+#if !defined(NO_AES) && defined(HAVE_AESGCM)
+	#if WOLFSSL_MIN_AUTH_TAG_SZ <= 8
+		case ENCR_AES_GCM_ICV8:
+			this->icv_size = 8;
+			break;
+	#endif
+	#if WOLFSSL_MIN_AUTH_TAG_SZ <= 12
+		case ENCR_AES_GCM_ICV12:
+			this->icv_size = 12;
+			break;
+	#endif
+		case ENCR_AES_GCM_ICV16:
+			this->icv_size = 16;
+			break;
+#endif
+#if !defined(NO_AES) && defined(HAVE_AESCCM)
+		case ENCR_AES_CCM_ICV8:
+			this->icv_size = 8;
+			break;
+		case ENCR_AES_CCM_ICV12:
+			this->icv_size = 12;
+			break;
+		case ENCR_AES_CCM_ICV16:
+			this->icv_size = 16;
+			break;
+#endif
+#if defined(HAVE_CHACHA) && defined(HAVE_POLY1305)
+		case ENCR_CHACHA20_POLY1305:
+			this->icv_size = 16;
+			break;
+#endif
+		default:
+			free(this);
+			return NULL;
+	}
+
+	switch (algo)
+	{
+#if !defined(NO_AES) && defined(HAVE_AESGCM)
+		case ENCR_AES_GCM_ICV8:
+		case ENCR_AES_GCM_ICV12:
+		case ENCR_AES_GCM_ICV16:
+			switch (key_size)
+			{
+				case 0:
+					key_size = 16;
+					/* FALL */
+				case 16:
+				case 24:
+				case 32:
+					this->iv_size = GCM_NONCE_LEN;
+					this->salt_len = GCM_SALT_LEN;
+					if (wc_AesInit(&this->cipher.aes, NULL, INVALID_DEVID) != 0)
+					{
+						DBG1(DBG_LIB, "AES Init failed, aead create failed");
+						free(this);
+						return NULL;
+					}
+					break;
+				default:
+					free(this);
+					return NULL;
+			}
+			break;
+#endif
+#if !defined(NO_AES) && defined(HAVE_AESCCM)
+		case ENCR_AES_CCM_ICV8:
+		case ENCR_AES_CCM_ICV12:
+		case ENCR_AES_CCM_ICV16:
+			switch (key_size)
+			{
+				case 0:
+					key_size = 16;
+					/* FALL */
+				case 16:
+				case 24:
+				case 32:
+					this->iv_size = CCM_NONCE_LEN;
+					this->salt_len = CCM_SALT_LEN;
+					if (wc_AesInit(&this->cipher.aes, NULL, INVALID_DEVID) != 0)
+					{
+						DBG1(DBG_LIB, "AES Init failed, aead create failed");
+						free(this);
+						return NULL;
+					}
+					break;
+				default:
+					free(this);
+					return NULL;
+			}
+			break;
+#endif
+#if defined(HAVE_CHACHA) && defined(HAVE_POLY1305)
+		case ENCR_CHACHA20_POLY1305:
+			switch (key_size)
+			{
+				case 0:
+					key_size = 32;
+					/* FALL */
+				case 32:
+					this->iv_size = CHACHA_IV_BYTES;
+					this->salt_len = 4;
+					break;
+				default:
+					free(this);
+					return NULL;
+			}
+			break;
+#endif
+		default:
+			free(this);
+			return NULL;
+	}
+
+	if (salt_size && salt_size != this->salt_len)
+	{
+		/* currently not supported */
+		free(this);
+		return NULL;
+	}
+
+	this->key = chunk_alloc(key_size);
+	this->iv_gen = iv_gen_seq_create();
+
+	return &this->public;
+}
+
+#endif

configure.ac
@@ -145,6 +145,7 @@ ARG_ENABL_SET([newhope],        [enable New Hope crypto plugin.])
 ARG_DISBL_SET([nonce],          [disable nonce generation plugin.])
 ARG_ENABL_SET([ntru],           [enables the NTRU crypto plugin.])
 ARG_ENABL_SET([openssl],        [enables the OpenSSL crypto plugin.])
+ARG_ENABL_SET([wolfssl],        [enables the wolfSSL crypto plugin.])
 ARG_ENABL_SET([padlock],        [enables VIA Padlock crypto plugin.])
 ARG_DISBL_SET([random],         [disable RNG implementation on top of /dev/(u)random.])
 ARG_DISBL_SET([rc2],            [disable RC2 software implementation plugin.])
@@ -444,7 +445,7 @@ if test x$imc_test = xtrue -o x$imv_test = xtrue -o x$imc_scanner = xtrue -o x$i
 fi
 
 if test x$fips_prf = xtrue; then
-	if test x$openssl = xfalse; then
+	if test x$openssl = xfalse -a x$wolfssl = xfalse; then
 		sha1=true;
 	fi
 fi
@@ -1136,6 +1137,14 @@ if test x$openssl = xtrue; then
 	AC_CHECK_HEADER([openssl/evp.h],,[AC_MSG_ERROR([OpenSSL header openssl/evp.h not found!])])
 fi
 
+if test x$wolfssl = xtrue; then
+	wolfssl_lib=wolfssl
+	AC_CHECK_LIB([$wolfssl_lib],[wolfSSL_Init],[LIBS="$LIBS"],
+		[AC_MSG_ERROR([wolfSSL lib$wolfssl_lib not found])],[$DLLIB])
+	AC_SUBST(WOLFSSL_LIB, [-l$wolfssl_lib])
+	AC_CHECK_HEADER([wolfssl/ssl.h],,[AC_MSG_ERROR([wolfSSL header wolfssl/ssl.h not found!])])
+fi
+
 if test x$gcrypt = xtrue; then
 	AC_CHECK_LIB([gcrypt],[gcry_control],[LIBS="$LIBS"],[AC_MSG_ERROR([gcrypt library not found])],[-lgpg-error])
 	AC_CHECK_HEADER([gcrypt.h],,[AC_MSG_ERROR([gcrypt header gcrypt.h not found!])])
@@ -1408,6 +1417,7 @@ ADD_PLUGIN([ipseckey],             [c charon])
 ADD_PLUGIN([pem],                  [s charon scepclient pki scripts manager medsrv attest nm cmd aikgen fuzz])
 ADD_PLUGIN([padlock],              [s charon])
 ADD_PLUGIN([openssl],              [s charon scepclient pki scripts manager medsrv attest nm cmd aikgen])
+ADD_PLUGIN([wolfssl],              [s charon scepclient pki scripts manager medsrv attest nm cmd aikgen])
 ADD_PLUGIN([gcrypt],               [s charon scepclient pki scripts manager medsrv attest nm cmd aikgen])
 ADD_PLUGIN([botan],                [s charon scepclient pki scripts manager medsrv attest nm cmd aikgen])
 ADD_PLUGIN([af-alg],               [s charon scepclient pki scripts medsrv attest nm cmd aikgen])
@@ -1578,6 +1588,7 @@ AM_CONDITIONAL(USE_MYSQL, test x$mysql = xtrue)
 AM_CONDITIONAL(USE_SQLITE, test x$sqlite = xtrue)
 AM_CONDITIONAL(USE_PADLOCK, test x$padlock = xtrue)
 AM_CONDITIONAL(USE_OPENSSL, test x$openssl = xtrue)
+AM_CONDITIONAL(USE_WOLFSSL, test x$wolfssl = xtrue)
 AM_CONDITIONAL(USE_GCRYPT, test x$gcrypt = xtrue)
 AM_CONDITIONAL(USE_BOTAN, test x$botan = xtrue)
 AM_CONDITIONAL(USE_AGENT, test x$agent = xtrue)
@@ -1857,6 +1868,7 @@ AC_CONFIG_FILES([
 	src/libstrongswan/plugins/sqlite/Makefile
 	src/libstrongswan/plugins/padlock/Makefile
 	src/libstrongswan/plugins/openssl/Makefile
+	src/libstrongswan/plugins/wolfssl/Makefile
 	src/libstrongswan/plugins/gcrypt/Makefile
 	src/libstrongswan/plugins/botan/Makefile
 	src/libstrongswan/plugins/agent/Makefile

src/libstrongswan/Makefile.am
@@ -558,6 +558,13 @@ if MONOLITHIC
 endif
 endif
 
+if USE_WOLFSSL
+  SUBDIRS += plugins/wolfssl
+if MONOLITHIC
+  libstrongswan_la_LIBADD += plugins/wolfssl/libstrongswan-wolfssl.la
+endif
+endif
+
 if USE_GCRYPT
   SUBDIRS += plugins/gcrypt
 if MONOLITHIC

src/libstrongswan/plugins/wolfssl/Makefile.am
@@ -1,0 +1,36 @@
+AM_CPPFLAGS = \
+	-I$(top_srcdir)/src/libstrongswan \
+	-DFIPS_MODE=${fips_mode}
+
+AM_CFLAGS = \
+	$(PLUGIN_CFLAGS)
+
+if MONOLITHIC
+noinst_LTLIBRARIES = libstrongswan-wolfssl.la
+else
+plugin_LTLIBRARIES = libstrongswan-wolfssl.la
+endif
+
+libstrongswan_wolfssl_la_SOURCES = \
+	wolfssl_common.h \
+	wolfssl_plugin.h wolfssl_plugin.c \
+	wolfssl_aead.h wolfssl_aead.c \
+	wolfssl_crypter.h wolfssl_crypter.c \
+	wolfssl_diffie_hellman.h wolfssl_diffie_hellman.c \
+	wolfssl_ec_diffie_hellman.h wolfssl_ec_diffie_hellman.c \
+	wolfssl_ec_private_key.h wolfssl_ec_private_key.c \
+	wolfssl_ec_public_key.h wolfssl_ec_public_key.c \
+	wolfssl_ed_private_key.h wolfssl_ed_private_key.c \
+	wolfssl_ed_public_key.h wolfssl_ed_public_key.c \
+	wolfssl_hasher.h wolfssl_hasher.c \
+	wolfssl_hmac.h wolfssl_hmac.c \
+	wolfssl_rsa_public_key.h wolfssl_rsa_public_key.c \
+	wolfssl_rsa_private_key.h wolfssl_rsa_private_key.c \
+	wolfssl_rng.h wolfssl_rng.c \
+	wolfssl_sha1_prf.h wolfssl_sha1_prf.c \
+	wolfssl_x_diffie_hellman.h wolfssl_x_diffie_hellman.c \
+	wolfssl_util.h wolfssl_util.c
+
+
+libstrongswan_wolfssl_la_LDFLAGS = -module -avoid-version
+libstrongswan_wolfssl_la_LIBADD  = $(WOLFSSL_LIB)

src/libstrongswan/plugins/wolfssl/wolfssl_aead.h
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2019 Sean Parkinson, wolfSSL Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Implements the aead_t interface using wolfSSL.
+ *
+ * @defgroup wolfssl_aead wolfssl_aead
+ * @{ @ingroup wolfssl_p
+ */
+
+#ifndef WOLFSSL_AEAD_H_
+#define WOLFSSL_AEAD_H_
+
+#include <crypto/aead.h>
+
+/**
+ * Constructor to create aead_t implementation.
+ *
+ * @param algo			algorithm to implement
+ * @param key_size		key size in bytes
+ * @param salt_size		size of implicit salt length
+ * @return				aead_t object, NULL if not supported
+ */
+aead_t *wolfssl_aead_create(encryption_algorithm_t algo, size_t key_size,
+							size_t salt_size);
+
+#endif /** WOLFSSL_AEAD_H_ @}*/

src/libstrongswan/plugins/wolfssl/wolfssl_common.h
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2019 Sean Parkinson, wolfSSL Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef WOLFSSL_COMMON_H_
+#define WOLFSSL_COMMON_H_
+
+#include <library.h>
+
+/* Undefine these as they are enum entries in wolfSSL - same values */
+#ifdef AES_BLOCK_SIZE
+#undef AES_BLOCK_SIZE
+#endif
+
+#ifdef CAMELLIA_BLOCK_SIZE
+#undef CAMELLIA_BLOCK_SIZE
+#endif
+
+#ifdef DES_BLOCK_SIZE
+#undef DES_BLOCK_SIZE
+#endif
+
+/* PARSE_ERROR is an enum entry in wolfSSL - not used in this plugin */
+#define PARSE_ERROR	WOLFSSL_PARSE_EROR
+
+#ifndef WOLFSSL_USER_SETTINGS
+	#include <wolfssl/options.h>
+#endif
+#include <wolfssl/ssl.h>
+
+#undef PARSE_ERROR
+
+#endif /* WOLFSSL_COMMON_H_ */

commit 1fe813f05ed12530be62d41788c44541df367d01

wolfssl: Fix memory leak on failed ICV verification if not using in-place decryption
Fixes: c92eade82c81 ("wolfssl: Add wolfSSL plugin for cryptographic implementations")

src/libstrongswan/plugins/wolfssl/wolfssl_aead.c
@@ -236,7 +236,10 @@ METHOD(aead_t, decrypt, bool,
 		default:
 			break;
 	}
-
+	if (!success && plain)
+	{
+		chunk_free(plain);
+	}
 	memwipe(nonce.ptr, nonce.len);
 	return success;
 }