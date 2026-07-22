rchildre3/strongswan

commit 23750961d5b9052ddfe6d4746435e84ca0c87a10

thread-value: Defer cleanup handling to thread termination on Windows
Instead of cleaning up all thread-values during destruction, cleanup handler
is invoked when a thread detaches. Thread detaching is cough using the Windows
DllMain() entry point, and allows us to basically revert 204098a7.

Using this mechanism, we make sure that the cleanup handler is invoked by the
the correct thread. Further, this mechanism works for externally-spawned
threads which run outside of our thread_cb() routine, and works more efficiently
with short-running threads.

src/libstrongswan/threading/windows/thread.c
@@ -246,36 +246,6 @@ void* thread_tls_remove(void *key)
 }
 
 /**
- * See header.
- */
-void thread_tls_remove_all(void *key)
-{
-	private_thread_t *thread;
-	enumerator_t *enumerator;
-	void *value;
-	bool old;
-
-	old = set_leak_detective(FALSE);
-	threads_lock->lock(threads_lock);
-
-	enumerator = threads->create_enumerator(threads);
-	while (enumerator->enumerate(enumerator, NULL, &thread))
-	{
-		value = thread->tls->remove(thread->tls, key);
-		if (value)
-		{
-			set_leak_detective(old);
-			thread_tls_cleanup(value);
-			set_leak_detective(FALSE);
-		}
-	}
-	enumerator->destroy(enumerator);
-
-	threads_lock->unlock(threads_lock);
-	set_leak_detective(old);
-}
-
-/**
  * Thread cleanup data
  */
 typedef struct {
@@ -632,6 +602,50 @@ void thread_exit(void *val)
 
 	end_thread(this);
 	ExitThread(0);
+}
+
+/**
+ * Clean up thread data while it detaches
+ */
+static void cleanup_tls()
+{
+	private_thread_t *this;
+	bool old;
+
+	old = set_leak_detective(FALSE);
+	threads_lock->lock(threads_lock);
+
+	this = threads->remove(threads, (void*)(uintptr_t)GetCurrentThreadId());
+
+	threads_lock->unlock(threads_lock);
+	set_leak_detective(old);
+
+	if (this)
+	{
+		/* If the thread exited, but has not been joined, it is in terminated
+		 * state. We must not mangle it, as we target externally spawned
+		 * threads only. */
+		if (!this->terminated && !this->detached)
+		{
+			destroy(this);
+		}
+	}
+}
+
+/**
+ * DllMain called for dll events
+ */
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	switch (fdwReason)
+	{
+		case DLL_THREAD_DETACH:
+			cleanup_tls();
+			break;
+		default:
+			break;
+	}
+	return TRUE;
 }
 
 /*

src/libstrongswan/threading/windows/thread.h
@@ -61,15 +61,6 @@ void* thread_tls_get(void *key);
 void* thread_tls_remove(void *key);
 
 /**
- * Remove a thread specific value from all threads.
- *
- * For each found TLS value thread_tls_cleanup() is invoked.
- *
- * @param key		unique key specifying the TLS variable
- */
-void thread_tls_remove_all(void *key);
-
-/**
  * Cleanup function for thread specific value.
  *
  * This is called whenever a thread exits to clean up thread specific data.

src/libstrongswan/threading/windows/thread_value.c
@@ -104,7 +104,13 @@ METHOD(thread_value_t, tls_get, void*,
 METHOD(thread_value_t, tls_destroy, void,
 	private_thread_value_t *this)
 {
-	thread_tls_remove_all(this);
+	entry_t *entry;
+
+	entry = thread_tls_remove(this);
+	if (entry)
+	{
+		thread_tls_cleanup(entry);
+	}
 	free(this);
 }
 

commit 1c6dd045eeb624a762a9c9a352ac93a7523dfb9e

windows: Avoid unnecessarily locking the global thread lock when removing TLS
Holding the lock could potentially cause a deadlock depending the
behavior of the called cleanup functions.  The TLS removal happens in
the context of the respective thread, so no locking is necessary.

Looks like removing these lines was missed when the referenced commit
partly reverted 204098a75202 ("thread-value: Immediately cleanup all
Windows TLS values on destroy"), which added the locking originally.

Fixes: 23750961d5b9 ("thread-value: Defer cleanup handling to thread termination on Windows")

src/libstrongswan/threading/windows/thread.c
@@ -238,9 +238,7 @@ void* thread_tls_remove(void *key)
 	thread = get_current_thread();
 
 	old = set_leak_detective(FALSE);
-	threads_lock->lock(threads_lock);
 	value = thread->tls->remove(thread->tls, key);
-	threads_lock->unlock(threads_lock);
 	set_leak_detective(old);
 
 	return value;
@@ -274,7 +272,6 @@ static void docleanup(private_thread_t *this)
 		set_leak_detective(FALSE);
 	}
 
-	threads_lock->lock(threads_lock);
 	enumerator = this->tls->create_enumerator(this->tls);
 	while (enumerator->enumerate(enumerator, NULL, &tls))
 	{
@@ -285,7 +282,6 @@ static void docleanup(private_thread_t *this)
 		set_leak_detective(FALSE);
 	}
 	enumerator->destroy(enumerator);
-	threads_lock->unlock(threads_lock);
 
 	set_leak_detective(old);
 }