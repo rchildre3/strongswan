rchildre3/strongswan

commit 11e7d0677c3ec6f241ba387d5aadba113b56e9be

socket-win: Install IKE bypass policies using bypass_socket()

src/libcharon/plugins/socket_win/socket_win_socket.c
@@ -19,6 +19,7 @@
 #include "socket_win_socket.h"
 
 #include <library.h>
+#include <hydra.h>
 #include <threading/thread.h>
 #include <daemon.h>
 
@@ -423,6 +424,16 @@ static SOCKET open_socket(private_socket_win_socket_t *this, int i)
 		DBG1(DBG_NET, "unable to set IP6_PKTINFO: %d", WSAGetLastError());
 		closesocket(s);
 		return INVALID_SOCKET;
+	}
+	if (!hydra->kernel_interface->bypass_socket(hydra->kernel_interface,
+												s, AF_INET))
+	{
+		DBG1(DBG_NET, "installing IPv4 IKE bypass policy failed");
+	}
+	if (!hydra->kernel_interface->bypass_socket(hydra->kernel_interface,
+												s, AF_INET6))
+	{
+		DBG1(DBG_NET, "installing IPv6 IKE bypass policy failed");
 	}
 	return s;
 }

src/libcharon/plugins/socket_win/socket_win_plugin.c
@@ -49,6 +49,7 @@ METHOD(plugin_t, get_features, int,
 	static plugin_feature_t f[] = {
 		PLUGIN_CALLBACK(socket_register, socket_win_socket_create),
 			PLUGIN_PROVIDE(CUSTOM, "socket"),
+				PLUGIN_DEPENDS(CUSTOM, "kernel-ipsec"),
 	};
 	*features = f;
 	return countof(f);

commit 9b76141b9083896700332b4adefa71a31c5b7f79

socket-win: Bind the sockets exclusively
This prevents other processes from binding the same ports and
misusing the protocol/port-specific bypass rules installed in WFP to
bypass the VPN.

Fixes: 11e7d0677c3e ("socket-win: Install IKE bypass policies using bypass_socket()")

src/libcharon/plugins/socket_win/socket_win_socket.c
@@ -339,7 +339,7 @@ static SOCKET open_socket(private_socket_win_socket_t *this, int i)
 		.sin6_port = htons(this->ports[i]),
 	};
 	int addrlen = sizeof(addr);
-	BOOL off = FALSE;
+	BOOL off = FALSE, on = TRUE;
 	DWORD dwon = TRUE;
 	SOCKET s;
 
@@ -347,6 +347,16 @@ static SOCKET open_socket(private_socket_win_socket_t *this, int i)
 	if (s == INVALID_SOCKET)
 	{
 		DBG1(DBG_NET, "creating socket failed: %d", WSAGetLastError());
+		return INVALID_SOCKET;
+	}
+	/* prevent other local processes from sharing the bound port and misusing
+	 * the bypass rules */
+	if (setsockopt(s, SOL_SOCKET, SO_EXCLUSIVEADDRUSE,
+				   (const char*)&on, sizeof(on)) == SOCKET_ERROR)
+	{
+		DBG1(DBG_NET, "unable to set SO_EXCLUSIVEADDRUSE: %d",
+			 WSAGetLastError());
+		closesocket(s);
 		return INVALID_SOCKET;
 	}
 	/* enable IPv4 on IPv6 socket */