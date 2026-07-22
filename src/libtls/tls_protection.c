rchildre3/strongswan

commit 7a2b02667c30cb3d3dbffc57ebca6466608492cd

libtls: Implement TLS 1.3 handshake on client-side
The code is a minimal handshake with the HelloRetryRequest message
implementation missing.
Can be tested with an OpenSSL server running TLS 1.3. The server must
be at least version 1.1.1 (September 2018).

Co-authored-by: ryru <pascal.knecht@hsr.ch>

src/libtls/tls_protection.c
@@ -73,23 +73,19 @@ METHOD(tls_protection_t, process, status_t,
 		return NEED_MORE;
 	}
 
-	if (this->aead_in)
+	if (this-> version < TLS_1_3 ||
+		type == TLS_APPLICATION_DATA)
 	{
-		if (!this->aead_in->decrypt(this->aead_in, this->version,
-									&type, this->seq_in, &data))
+		if (this->aead_in)
 		{
-			DBG1(DBG_TLS, "TLS record decryption failed");
-			this->alert->add(this->alert, TLS_FATAL, TLS_BAD_RECORD_MAC);
-			return NEED_MORE;
+			if (!this->aead_in->decrypt(this->aead_in, this->version,
+										&type, this->seq_in, &data))
+			{
+				DBG1(DBG_TLS, "TLS record decryption failed");
+				this->alert->add(this->alert, TLS_FATAL, TLS_BAD_RECORD_MAC);
+				return NEED_MORE;
+			}
 		}
-	}
-
-	if (type == TLS_CHANGE_CIPHER_SPEC)
-	{
-		this->seq_in = 0;
-	}
-	else
-	{
 		this->seq_in++;
 	}
 	return this->compression->process(this->compression, type, data);
@@ -103,9 +99,8 @@ METHOD(tls_protection_t, build, status_t,
 	status = this->compression->build(this->compression, type, data);
 	if (status == NEED_MORE)
 	{
-		if (*type == TLS_CHANGE_CIPHER_SPEC)
+		if (this-> version < TLS_1_3 && *type == TLS_CHANGE_CIPHER_SPEC)
 		{
-			this->seq_out = 0;
 			return status;
 		}
 		if (this->aead_out)
@@ -129,10 +124,12 @@ METHOD(tls_protection_t, set_cipher, void,
 	if (inbound)
 	{
 		this->aead_in = aead;
+		this->seq_in = 0;
 	}
 	else
 	{
 		this->aead_out = aead;
+		this->seq_out = 0;
 	}
 }
 

scripts/tls_test.c
@@ -92,6 +92,7 @@ static int run_client(host_t *host, identification_t *server,
 
 	while (times == -1 || times-- > 0)
 	{
+		/* Open IPv4 socket */
 		fd = socket(AF_INET, SOCK_STREAM, 0);
 		if (fd == -1)
 		{
@@ -101,11 +102,21 @@ static int run_client(host_t *host, identification_t *server,
 		if (connect(fd, host->get_sockaddr(host),
 					*host->get_sockaddr_len(host)) == -1)
 		{
-			DBG1(DBG_TLS, "connecting to %#H failed: %s", host, strerror(errno));
-			close(fd);
-			return 1;
+			/* Check if the socket should use IPv6 instead */
+			fd = socket(AF_INET6, SOCK_STREAM, 0);
+			{
+				if (connect(fd, host->get_sockaddr(host),
+				            *host->get_sockaddr_len(host)) == -1)
+				{
+					DBG1(DBG_TLS, "connecting to %#H failed: %s", host, strerror(errno));
+					close(fd);
+					return 1;
+				}
+			}
+
 		}
-		tls = tls_socket_create(FALSE, server, client, fd, cache, TLS_1_2, TRUE);
+		tls = tls_socket_create(FALSE, server, client, fd, cache,
+							    TLS_1_3, TRUE);
 		if (!tls)
 		{
 			close(fd);
@@ -337,11 +348,17 @@ int main(int argc, char *argv[])
 		usage(stderr, argv[0]);
 		return 1;
 	}
-	host = host_create_from_dns(address, 0, port);
+	/* Try to connect with IPv4 first*/
+	host = host_create_from_dns(address, AF_INET, port);
 	if (!host)
 	{
-		DBG1(DBG_TLS, "resolving hostname %s failed", address);
-		return 1;
+		/* If it fails, try IPv6*/
+		host = host_create_from_dns(address, AF_INET6, port);
+		if (!host)
+		{
+			DBG1(DBG_TLS, "resolving hostname %s failed", address);
+			return 1;
+		}
 	}
 	server = identification_create_from_string(address);
 	cache = tls_cache_create(100, 30);

src/libtls/tls.c
@@ -26,12 +26,13 @@
 
 ENUM_BEGIN(tls_version_names, SSL_2_0, SSL_2_0,
 	"SSLv2");
-ENUM_NEXT(tls_version_names, SSL_3_0, TLS_1_2, SSL_2_0,
+ENUM_NEXT(tls_version_names, SSL_3_0, TLS_1_3, SSL_2_0,
 	"SSLv3",
 	"TLS 1.0",
 	"TLS 1.1",
-	"TLS 1.2");
-ENUM_END(tls_version_names, TLS_1_2);
+	"TLS 1.2",
+	"TLS 1.3");
+ENUM_END(tls_version_names, TLS_1_3);
 
 ENUM(tls_content_type_names, TLS_CHANGE_CIPHER_SPEC, TLS_APPLICATION_DATA,
 	"ChangeCipherSpec",
@@ -40,12 +41,22 @@ ENUM(tls_content_type_names, TLS_CHANGE_CIPHER_SPEC, TLS_APPLICATION_DATA,
 	"ApplicationData",
 );
 
-ENUM_BEGIN(tls_handshake_type_names, TLS_HELLO_REQUEST, TLS_SERVER_HELLO,
-	"HelloRequest",
-	"ClientHello",
-	"ServerHello");
+ENUM_BEGIN(tls_handshake_type_names, TLS_HELLO_REQUEST, TLS_HELLO_REQUEST,
+	"HelloRequest");
 ENUM_NEXT(tls_handshake_type_names,
-		TLS_CERTIFICATE, TLS_CLIENT_KEY_EXCHANGE, TLS_SERVER_HELLO,
+		TLS_CLIENT_HELLO, TLS_HELLO_RETRY_REQUEST, TLS_HELLO_REQUEST,
+	"ClientHello",
+	"ServerHello",
+	"HelloVerifyRequest",
+	"NewSessionTicket",
+	"EndOfEarlyData",
+	"HelloRetryRequest");
+ENUM_NEXT(tls_handshake_type_names,
+		TLS_ENCRYPTED_EXTENSIONS, TLS_ENCRYPTED_EXTENSIONS,
+		TLS_HELLO_RETRY_REQUEST,
+	"EncryptedExtensions");
+ENUM_NEXT(tls_handshake_type_names,
+		TLS_CERTIFICATE, TLS_CLIENT_KEY_EXCHANGE, TLS_ENCRYPTED_EXTENSIONS,
 	"Certificate",
 	"ServerKeyExchange",
 	"CertificateRequest",
@@ -53,9 +64,16 @@ ENUM_NEXT(tls_handshake_type_names,
 	"CertificateVerify",
 	"ClientKeyExchange");
 ENUM_NEXT(tls_handshake_type_names,
-		TLS_FINISHED, TLS_FINISHED, TLS_CLIENT_KEY_EXCHANGE,
-	"Finished");
-ENUM_END(tls_handshake_type_names, TLS_FINISHED);
+		  TLS_FINISHED, TLS_KEY_UPDATE, TLS_CLIENT_KEY_EXCHANGE,
+	"Finished",
+	"CertificateUrl",
+	"CertificateStatus",
+	"SupplementalData",
+	"KeyUpdate");
+ENUM_NEXT(tls_handshake_type_names,
+		TLS_MESSAGE_HASH, TLS_MESSAGE_HASH, TLS_KEY_UPDATE,
+	"MessageHash");
+ENUM_END(tls_handshake_type_names, TLS_MESSAGE_HASH);
 
 ENUM_BEGIN(tls_extension_names, TLS_EXT_SERVER_NAME, TLS_EXT_STATUS_REQUEST,
 	"server name",
@@ -65,17 +83,42 @@ ENUM_BEGIN(tls_extension_names, TLS_EXT_SERVER_NAME, TLS_EXT_STATUS_REQUEST,
 	"truncated hmac",
 	"status request");
 ENUM_NEXT(tls_extension_names,
-		TLS_EXT_ELLIPTIC_CURVES, TLS_EXT_EC_POINT_FORMATS,
+		TLS_EXT_SUPPORTED_GROUPS, TLS_EXT_EC_POINT_FORMATS,
 		TLS_EXT_STATUS_REQUEST,
-	"elliptic curves",
+	"supported groups",
 	"ec point formats");
 ENUM_NEXT(tls_extension_names,
-		TLS_EXT_SIGNATURE_ALGORITHMS, TLS_EXT_SIGNATURE_ALGORITHMS,
+		TLS_EXT_SIGNATURE_ALGORITHMS,
+		TLS_EXT_APPLICATION_LAYER_PROTOCOL_NEGOTIATION,
 		TLS_EXT_EC_POINT_FORMATS,
-	"signature algorithms");
+	"signature algorithms",
+	"use rtp",
+	"heartbeat",
+	"application layer protocol negotiation");
+ENUM_NEXT(tls_extension_names,
+		TLS_CLIENT_CERTIFICATE_TYPE, TLS_SERVER_CERTIFICATE_TYPE,
+		TLS_EXT_APPLICATION_LAYER_PROTOCOL_NEGOTIATION,
+	"client certificate type",
+	"server certificate type");
+ENUM_NEXT(tls_extension_names,
+		TLS_EXT_PRE_SHARED_KEY, TLS_EXT_PSK_KEY_EXCHANGE_MODES,
+		TLS_SERVER_CERTIFICATE_TYPE,
+	"pre-shared key",
+	"early data",
+	"supported versions",
+	"cookie",
+	"psk key exchange modes");
+ENUM_NEXT(tls_extension_names,
+		TLS_EXT_CERTIFICATE_AUTHORITIES, TLS_EXT_KEY_SHARE,
+		TLS_EXT_PSK_KEY_EXCHANGE_MODES,
+	"certificate authorities",
+	"oid filters",
+	"post-handshake auth",
+	"signature algorithms cert",
+	"key-share");
 ENUM_NEXT(tls_extension_names,
 		TLS_EXT_RENEGOTIATION_INFO, TLS_EXT_RENEGOTIATION_INFO,
-		TLS_EXT_SIGNATURE_ALGORITHMS,
+		TLS_EXT_KEY_SHARE,
 	"renegotiation info");
 ENUM_END(tls_extension_names, TLS_EXT_RENEGOTIATION_INFO);
 
@@ -107,9 +150,14 @@ struct private_tls_t {
 	bool is_server;
 
 	/**
-	 * Negotiated TLS version
+	 * Negotiated TLS version and maximum supported TLS version
 	 */
-	tls_version_t version;
+	tls_version_t version_max;
+
+	/**
+	 * Minimal supported TLS version
+	 */
+	tls_version_t version_min;
 
 	/**
 	 * TLS stack purpose, as given to constructor
@@ -300,7 +348,14 @@ METHOD(tls_t, build, status_t,
 			{
 				case NEED_MORE:
 					record.type = type;
-					htoun16(&record.version, this->version);
+					if (this->version_max < TLS_1_3)
+					{
+						htoun16(&record.version, this->version_max);
+					}
+					else
+					{
+						htoun16(&record.version, TLS_1_2);
+					}
 					htoun16(&record.length, data.len);
 					this->output = chunk_cat("mcm", this->output,
 											 chunk_from_thing(record), data);
@@ -361,16 +416,22 @@ METHOD(tls_t, get_peer_id, identification_t*,
 	return this->handshake->get_peer_id(this->handshake);
 }
 
-METHOD(tls_t, get_version, tls_version_t,
+METHOD(tls_t, get_version_max, tls_version_t,
 	private_tls_t *this)
 {
-	return this->version;
+	return this->version_max;
+}
+
+METHOD(tls_t, get_version_min, tls_version_t,
+	private_tls_t *this)
+{
+	return this->version_min;
 }
 
 METHOD(tls_t, set_version, bool,
 	private_tls_t *this, tls_version_t version)
 {
-	if (version > this->version)
+	if (version > this->version_max)
 	{
 		return FALSE;
 	}
@@ -379,7 +440,8 @@ METHOD(tls_t, set_version, bool,
 		case TLS_1_0:
 		case TLS_1_1:
 		case TLS_1_2:
-			this->version = version;
+		case TLS_1_3:
+			this->version_max = version;
 			this->protection->set_version(this->protection, version);
 			return TRUE;
 		case SSL_2_0:
@@ -466,7 +528,8 @@ tls_t *tls_create(bool is_server, identification_t *server,
 			.is_server = _is_server,
 			.get_server_id = _get_server_id,
 			.get_peer_id = _get_peer_id,
-			.get_version = _get_version,
+			.get_version_max = _get_version_max,
+			.get_version_min = _get_version_min,
 			.set_version = _set_version,
 			.get_purpose = _get_purpose,
 			.is_complete = _is_complete,
@@ -475,7 +538,8 @@ tls_t *tls_create(bool is_server, identification_t *server,
 			.destroy = _destroy,
 		},
 		.is_server = is_server,
-		.version = TLS_1_2,
+		.version_max = TLS_1_3,
+		.version_min = TLS_1_0,
 		.application = application,
 		.purpose = purpose,
 	);

src/libtls/tls.h
@@ -52,6 +52,7 @@ enum tls_version_t {
 	TLS_1_0 = 0x0301,
 	TLS_1_1 = 0x0302,
 	TLS_1_2 = 0x0303,
+	TLS_1_3 = 0x0304,
 };
 
 /**
@@ -81,6 +82,11 @@ enum tls_handshake_type_t {
 	TLS_HELLO_REQUEST = 0,
 	TLS_CLIENT_HELLO = 1,
 	TLS_SERVER_HELLO = 2,
+	TLS_HELLO_VERIFY_REQUEST = 3,
+	TLS_NEW_SESSION_TICKET = 4,
+	TLS_END_OF_EARLY_DATA = 5,
+	TLS_HELLO_RETRY_REQUEST = 6,
+	TLS_ENCRYPTED_EXTENSIONS = 8,
 	TLS_CERTIFICATE = 11,
 	TLS_SERVER_KEY_EXCHANGE = 12,
 	TLS_CERTIFICATE_REQUEST = 13,
@@ -88,6 +94,11 @@ enum tls_handshake_type_t {
 	TLS_CERTIFICATE_VERIFY = 15,
 	TLS_CLIENT_KEY_EXCHANGE = 16,
 	TLS_FINISHED = 20,
+	TLS_CERTIFICATE_URL = 21,
+	TLS_CERTIFICATE_STATUS = 22,
+	TLS_SUPPLEMENTAL_DATA = 23,
+	TLS_KEY_UPDATE = 24,
+	TLS_MESSAGE_HASH = 254,
 };
 
 /**
@@ -114,7 +125,7 @@ enum tls_purpose_t {
 };
 
 /**
- * TLS Hello extension types.
+ * TLS Handshake extension types.
  */
 enum tls_extension_t {
 	/** Server name the client wants to talk to */
@@ -129,12 +140,42 @@ enum tls_extension_t {
 	TLS_EXT_TRUNCATED_HMAC = 4,
 	/** list of OCSP responders the client trusts */
 	TLS_EXT_STATUS_REQUEST = 5,
-	/** list of supported elliptic curves */
-	TLS_EXT_ELLIPTIC_CURVES = 10,
+	/** list of supported groups, in legacy tls: elliptic curves */
+	TLS_EXT_SUPPORTED_GROUPS = 10,
 	/** supported point formats */
 	TLS_EXT_EC_POINT_FORMATS = 11,
 	/** list supported signature algorithms */
 	TLS_EXT_SIGNATURE_ALGORITHMS = 13,
+	/** indicate usage of Datagram Transport Layer Security (DTLS) */
+	TLS_EXT_USE_SRTP = 14,
+	/** indicate usage of heartbeat */
+	TLS_EXT_HEARTBEAT = 15,
+	/** indicate usage of application-layer protocol negotiation */
+	TLS_EXT_APPLICATION_LAYER_PROTOCOL_NEGOTIATION = 16,
+	/** exchange raw public key, client side*/
+	TLS_CLIENT_CERTIFICATE_TYPE = 19,
+	/** exchange raw public key, server side*/
+	TLS_SERVER_CERTIFICATE_TYPE = 20,
+	/** negotiate identity of the psk **/
+	TLS_EXT_PRE_SHARED_KEY = 41,
+	/** send data in 0-RTT when psk is used and early data is allowed **/
+	TLS_EXT_EARLY_DATA = 42,
+	/** negotiate supported tls versions **/
+	TLS_EXT_SUPPORTED_VERSIONS = 43,
+	/** identify client **/
+	TLS_EXT_COOKIE = 44,
+	/** psk modes supported by the client **/
+	TLS_EXT_PSK_KEY_EXCHANGE_MODES = 45,
+	/** indicate supported ca's by endpoint **/
+	TLS_EXT_CERTIFICATE_AUTHORITIES = 47,
+	/** provide oid/value pairs to match client's certificate **/
+	TLS_EXT_OID_FILTERS = 48,
+	/** willing to perform post-handshake authentication **/
+	TLS_EXT_POST_HANDSHAKE_AUTH = 49,
+	/** list supported signature algorithms to verify certificates **/
+	TLS_EXT_SIGNATURE_ALGORITHMS_CERT = 50,
+	/** list endpoint's cryptographic parameters **/
+	TLS_EXT_KEY_SHARE = 51,
 	/** cryptographic binding for RFC 5746 renegotiation indication */
 	TLS_EXT_RENEGOTIATION_INFO = 65281,
 };
@@ -216,11 +257,18 @@ struct tls_t {
 	identification_t* (*get_peer_id)(tls_t *this);
 
 	/**
-	 * Get the negotiated TLS/SSL version.
+	 * Get the maximum and negotiated TLS/SSL version.
 	 *
-	 * @return			negotiated TLS version
+	 * @return			max and negotiated TLS version
 	 */
-	tls_version_t (*get_version)(tls_t *this);
+	tls_version_t (*get_version_max)(tls_t *this);
+
+	/**
+	* Get the minimum TLS/SSL version.
+	*
+	* @return			min TLS version
+	*/
+	tls_version_t (*get_version_min)(tls_t *this);
 
 	/**
 	 * Set the negotiated TLS/SSL version.

src/libtls/tls_alert.c
@@ -63,19 +63,34 @@ ENUM_NEXT(tls_alert_desc_names, TLS_INTERNAL_ERROR, TLS_INTERNAL_ERROR,
 		TLS_INSUFFICIENT_SECURITY,
 	"internal error",
 );
+ENUM_NEXT(tls_alert_desc_names, TLS_INAPPROPRIATE_FALLBACK,
+		TLS_INAPPROPRIATE_FALLBACK, TLS_INTERNAL_ERROR,
+	"inappropriate fallback",
+);
 ENUM_NEXT(tls_alert_desc_names, TLS_USER_CANCELED, TLS_USER_CANCELED,
-		TLS_INTERNAL_ERROR,
+		TLS_INAPPROPRIATE_FALLBACK,
 	"user canceled",
 );
 ENUM_NEXT(tls_alert_desc_names, TLS_NO_RENEGOTIATION, TLS_NO_RENEGOTIATION,
 		TLS_USER_CANCELED,
 	"no renegotiation",
 );
-ENUM_NEXT(tls_alert_desc_names, TLS_UNSUPPORTED_EXTENSION, TLS_UNSUPPORTED_EXTENSION,
+ENUM_NEXT(tls_alert_desc_names, TLS_MISSING_EXTENSION, TLS_CERTIFICATE_REQUIRED,
 		TLS_NO_RENEGOTIATION,
+	"missing extensions",
 	"unsupported extension",
+	"certificate unobtainable",
+	"recognized name",
+	"bad certificate status response",
+	"bad certificate hash value",
+	"unknown psk identity",
+	"certificate required",
 );
-ENUM_END(tls_alert_desc_names, TLS_UNSUPPORTED_EXTENSION);
+ENUM_NEXT(tls_alert_desc_names, TLS_NO_APPLICATION_PROTOCOL,
+		TLS_NO_APPLICATION_PROTOCOL, TLS_CERTIFICATE_REQUIRED,
+	"no application protocol"
+);
+ENUM_END(tls_alert_desc_names, TLS_NO_APPLICATION_PROTOCOL);
 
 
 typedef struct private_tls_alert_t private_tls_alert_t;

src/libtls/tls_alert.h
@@ -61,9 +61,18 @@ enum tls_alert_desc_t {
 	TLS_PROTOCOL_VERSION = 70,
 	TLS_INSUFFICIENT_SECURITY = 71,
 	TLS_INTERNAL_ERROR = 80,
+	TLS_INAPPROPRIATE_FALLBACK = 86,
 	TLS_USER_CANCELED = 90,
 	TLS_NO_RENEGOTIATION = 100,
+	TLS_MISSING_EXTENSION = 109,
 	TLS_UNSUPPORTED_EXTENSION = 110,
+	TLS_CERTIFICATE_UNOBTAINABLE = 111,
+	TLS_RECOGNIZED_NAME = 112,
+	TLS_BAD_CERTIFICATE_STATUS_RESPONSE = 113,
+	TLS_BAD_CERTIFICATE_HASH_VALUE = 114,
+	TLS_UNKNOWN_PSK_IDENTITY = 115,
+	TLS_CERTIFICATE_REQUIRED = 116,
+	TLS_NO_APPLICATION_PROTOCOL = 120,
 };
 
 /**

src/libtls/tls_crypto.h
@@ -27,7 +27,7 @@ typedef enum tls_hash_algorithm_t tls_hash_algorithm_t;
 typedef enum tls_signature_algorithm_t tls_signature_algorithm_t;
 typedef enum tls_client_certificate_type_t tls_client_certificate_type_t;
 typedef enum tls_ecc_curve_type_t tls_ecc_curve_type_t;
-typedef enum tls_named_curve_t tls_named_curve_t;
+typedef enum tls_named_group_t tls_named_group_t;
 typedef enum tls_ansi_point_format_t tls_ansi_point_format_t;
 typedef enum tls_ec_point_format_t tls_ec_point_format_t;
 
@@ -191,6 +191,12 @@ enum tls_cipher_suite_t {
 
 	TLS_EMPTY_RENEGOTIATION_INFO_SCSV =			0x00FF,
 
+	TLS_AES_128_GCM_SHA256 = 					0x1301,
+	TLS_AES_256_GCM_SHA384 = 					0x1302,
+	TLS_CHACHA20_POLY1305_SHA256 =				0x1303,
+	TLS_AES_128_CCM_SHA256 = 					0x1304,
+	TLS_AES_128_CCM_8_SHA256 = 					0x1305,
+
 	TLS_ECDH_ECDSA_WITH_NULL_SHA =				0xC001,
 	TLS_ECDH_ECDSA_WITH_RC4_128_SHA =			0xC002,
 	TLS_ECDH_ECDSA_WITH_3DES_EDE_CBC_SHA =		0xC003,
@@ -327,7 +333,7 @@ extern enum_name_t *tls_ecc_curve_type_names;
 /**
  * TLS Named Curve identifiers
  */
-enum tls_named_curve_t {
+enum tls_named_group_t {
 	TLS_SECT163K1 =		 1,
 	TLS_SECT163R1 =		 2,
 	TLS_SECT163R2 =		 3,
@@ -353,12 +359,21 @@ enum tls_named_curve_t {
 	TLS_SECP256R1 =		23,
 	TLS_SECP384R1 =		24,
 	TLS_SECP521R1 =		25,
+
+	/* TLS 1.3: new ecdhe, dhe groups */
+	TLS_CURVE25519 =    29,
+	TLS_CURVE_448  =    30,
+	TLS_FFDHE2048  =    256,
+	TLS_FFDHE3072  =    257,
+	TLS_FFDHE4096  =    258,
+	TLS_FFDHE6144  =    259,
+	TLS_FFDHE8192  =    260,
 };
 
 /**
- * Enum names for tls_named_curve_t
+ * Enum names for tls_named_group_t
  */
-extern enum_name_t *tls_named_curve_names;
+extern enum_name_t *tls_named_group_names;
 
 /**
  * EC Point format, ANSI X9.62.
@@ -432,7 +447,7 @@ struct tls_crypto_t {
 	/**
 	 * Create an enumerator over supported ECDH groups.
 	 *
-	 * Enumerates over (diffie_hellman_group_t, tls_named_curve_t)
+	 * Enumerates over (diffie_hellman_group_t, tls_named_group_t)
 	 *
 	 * @return				enumerator
 	 */
@@ -499,13 +514,22 @@ struct tls_crypto_t {
 							 bio_reader_t *reader);
 
 	/**
-	 * Calculate the data of a TLS finished message.
+	 * Calculate the data of a legacyTLS finished message.
 	 *
 	 * @param label			ASCII label to use for calculation
 	 * @param out			buffer to write finished data to
 	 * @return				TRUE if calculation successful
 	 */
 	bool (*calculate_finished)(tls_crypto_t *this, char *label, char out[12]);
+
+	/**
+	 * Calculate the data of a TLS finished message.
+	 *
+	 * @param out			buffer to write finished data to
+	 * @return				TRUE if calculation successful
+	 */
+	bool (*calculate_finished_tls13)(tls_crypto_t *this, bool is_server,
+									 chunk_t *out);
 
 	/**
 	 * Derive the master secret, MAC and encryption keys.
@@ -520,6 +544,21 @@ struct tls_crypto_t {
 	bool (*derive_secrets)(tls_crypto_t *this, chunk_t premaster,
 						   chunk_t session, identification_t *id,
 						   chunk_t client_random, chunk_t server_random);
+
+	/**
+	 * Derive the handshake keys.
+	 *
+	 * @param shared_secret 	input key material
+	 * @return 					TRUE if	secret derived successfully
+	 */
+	bool (*derive_handshake_secret)(tls_crypto_t *this, chunk_t shared_secret);
+
+	/**
+	 * Derive the application keys.
+	 *
+	 * @return 					TRUE if	secret derived successfully
+	 */
+	bool (*derive_app_secret)(tls_crypto_t *this);
 
 	/**
 	 * Try to resume a TLS session, derive key material.

commit f4074c3d7b634225bdb6bcda86a18758bb4a8ae7

tls-protection: Reject unencrypted TLS 1.3 records
We only allow unencrypted change_cipher_spec records (as before), which
are handled by the upper layers accordingly.  Without the check, we
also accepted unprotected alerts and handshake records that could
potentially cause state confusion.

Fixes: 7a2b02667c30 ("libtls: Implement TLS 1.3 handshake on client-side")

src/libtls/tls_protection.c
@@ -74,6 +74,15 @@ METHOD(tls_protection_t, process, status_t,
 		return NEED_MORE;
 	}
 
+	if (this->version >= TLS_1_3 && this->aead_in &&
+		type != TLS_APPLICATION_DATA && type != TLS_CHANGE_CIPHER_SPEC)
+	{
+		DBG1(DBG_TLS, "received unencrypted TLS 1.3 %N record",
+			 tls_content_type_names, type);
+		this->alert->add(this->alert, TLS_FATAL, TLS_UNEXPECTED_MESSAGE);
+		return NEED_MORE;
+	}
+
 	if (this->version < TLS_1_3 ||
 		type == TLS_APPLICATION_DATA)
 	{